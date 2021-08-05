5e2aebeffab0e57a4ea83ca9ab33d380eacad00d
sanantoha <sanantoha@gmail.com>
2021-08-06 01:47:18 +0400

    FourSome is done

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -6,7 +6,6 @@ import java.util.stream.IntStream;
 public class Test {
     public static void main(String[] args) {
         /*
-            FourSum
             SameTree
             PhoneNumberMnemonic
             BFSTreeTraverse
