6c458537f33ece95aab0a0722cb0336c0cbbd45a
sanantoha <sanantoha@gmail.com>
2021-08-07 16:58:39 +0400

    optimaze MergeSort

--- src/main/java/sorting/MergeSort.java
+++ src/main/java/sorting/MergeSort.java
@@ -5,44 +5,41 @@ import java.util.Random;
 
 public class MergeSort {
 
-    public static void mergeSort(int[] arr, int lo, int hi) {
-        if (lo >= hi) return;
-        int q = (lo + hi) >>> 1;
-        mergeSort(arr, lo, q);
-        mergeSort(arr, q + 1, hi);
-        merge(arr, lo, q, hi);
+    public static int[] mergeSort(int[] array) {
+        int[] result = array.clone();
+        sort(array, 0, array.length - 1, result);
+        return result;
     }
 
-    public static void merge(int[] arr, int lo, int q, int hi) {
-        int[] res = new int[hi - lo + 1];
-        int i = lo;
-        int j = q + 1;
-        int k = 0;
-        while (i <= q && j <= hi) {
-            if (arr[i] <= arr[j]) {
-                res[k] = arr[i];
-                i++;
+    private static void sort(int[] array, int l, int r, int[] result) {
+        if (l >= r) return;
+        int mid = (l + r) >>> 1;
+
+        sort(result, l, mid, array);
+        sort(result, mid + 1, r, array);
+        merge(array, l, mid, r, result);
+    }
+
+    private static void merge(int[] array, int l, int mid, int r, int[] result) {
+        int i = l;
+        int j = mid + 1;
+        int k = l;
+        while (i <= mid && j <= r) {
+            if (array[i] <= array[j]) {
+                result[k++] = array[i++];
             } else {
-                res[k] = arr[j];
-                j++;
+                result[k++] = array[j++];
             }
-            k++;
         }
 
-        while(i <= q) res[k++] = arr[i++];
-        while(j <= hi) res[k++] = arr[j++];
-
-        for (int z = 0; z < res.length; z++) {
-            arr[lo + z] = res[z];
-        }
+        while (i <= mid) result[k++] = array[i++];
+        while (j <= r) result[k++] = array[j++];
     }
 
     public static void main(String[] args) {
         int[] arr = {8,3,6,8,3,1,5,7,8,9};
 
-        mergeSort(arr, 0, arr.length - 1);
-
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(mergeSort(arr)));
 
 
         System.out.println("=========================");
@@ -53,7 +50,6 @@ public class MergeSort {
         }
 
         System.out.println(Arrays.toString(arr1));
-        mergeSort(arr1, 0, arr1.length - 1);
-        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(mergeSort(arr1)));
     }
 }
