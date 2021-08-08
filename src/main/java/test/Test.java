bccf25ccff0b659c4f579daea03bff37e36de63f
sanantoha <sanantoha@gmail.com>
2021-08-08 21:49:55 +0400

    add one more task for everyday plan.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -6,6 +6,7 @@ import java.util.stream.IntStream;
 public class Test {
     public static void main(String[] args) {
         /*
+            ThreeSum
             MinRewards - https://www.algoexpert.io/questions/Min%20Rewards
             ReverseLinkedList
             PalindromSubstrings
@@ -31,18 +32,5 @@ public class Test {
             LargestRange
             MergeIntervals
          */
-
-        int[] arr = {1,2,3,4,5,6, 7};
-
-        System.out.println(Arrays.stream(arr).sum());
-
-        System.out.println(IntStream.of(arr).sum());
-
-        PriorityQueue<int[]> queue =
-                new PriorityQueue(3, Comparator.comparingInt((int[] o) -> o[1]));
-
-        queue.add(new int[] {1, 2});
-
-        System.out.println(queue);
     }
 }
