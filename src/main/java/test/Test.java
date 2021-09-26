075d4259973497411138a811c193b12323e0d8ef
sanantoha <sanantoha@gmail.com>
2021-09-26 15:04:38 +0400

    3 tasks. 26.09.2021.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -7,9 +7,6 @@ import java.util.stream.IntStream;
 public class Test {
     public static void main(String[] args) {
         /*
-            ValidateBinarySearchTree
-            MergeSort
-            CloneGraph
             MinHeap
             Powerset
             FindClosestValueInBST
@@ -89,6 +86,9 @@ public class Test {
             StringWithoutAAAorBBB
             Pow
             SelectSort
+            ValidateBinarySearchTree
+            MergeSort
+            CloneGraph
          */
     }
 
