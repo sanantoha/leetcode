e0dad325dd1d2d1e1e61d59efa825f34b91d88b2
sanantoha <sanantoha@gmail.com>
2021-10-31 00:45:06 +0400

    prep. 30.10.2021.

---
+++ src/main/java/dynamic/LongestNonDecrSubseq9.java
@@ -0,0 +1,21 @@
+package dynamic;
+
+import java.util.List;
+
+public class LongestNonDecrSubseq9 {
+
+    public static int lnds(int[] arr) {
+        return -1;
+    }
+
+    public static List<Integer> lndsList(int[] arr) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2,-1,3,4,5,2,2,2,2};
+
+        System.out.println(lnds(arr));
+        System.out.println(lndsList(arr));
+    }
+}
