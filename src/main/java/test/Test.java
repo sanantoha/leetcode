1498a2ead9e22277529b8ddff6c69dc64770b687
sanantoha <sanantoha@gmail.com>
2021-12-04 14:42:41 +0400

    +3 tasks. 04.12.2021.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -8,6 +8,7 @@ public class Test {
             LRUCache
             UniquePaths
             SameBSTs
+            MaxPathSumInBinaryTree
             Pow
             TopologicalSortDFSCycleGraph
             LevenshteinDistance
---
+++ src/main/java/tree/MaxPathSumInBinaryTree.java
@@ -0,0 +1,57 @@
+package tree;
+
+import java.util.ArrayDeque;
+
+/**
+ * https://www.algoexpert.io/questions/Max%20Path%20Sum%20In%20Binary%20Tree
+ */
+public class MaxPathSumInBinaryTree {
+
+    static class BinaryTree {
+        public int value;
+        public BinaryTree left;
+        public BinaryTree right;
+
+        public BinaryTree(int value) {
+            this.value = value;
+        }
+    }
+
+    public static int maxPathSum(BinaryTree tree) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        TestBinaryTree test = new TestBinaryTree(1);
+        test.insert(new int[] {2, 3, 4, 5, 6, 7}, 0);
+        System.out.println(maxPathSum(test) == 18);
+    }
+
+    static class TestBinaryTree extends BinaryTree {
+        public TestBinaryTree(int value) {
+            super(value);
+        }
+
+        public void insert(int[] values, int i) {
+            if (i >= values.length) {
+                return;
+            }
+            ArrayDeque<BinaryTree> queue = new ArrayDeque<BinaryTree>();
+            queue.addLast(this);
+            while (queue.size() > 0) {
+                BinaryTree current = queue.pollFirst();
+                if (current.left == null) {
+                    current.left = new BinaryTree(values[i]);
+                    break;
+                }
+                queue.addLast(current.left);
+                if (current.right == null) {
+                    current.right = new BinaryTree(values[i]);
+                    break;
+                }
+                queue.addLast(current.right);
+            }
+            insert(values, i + 1);
+        }
+    }
+}
