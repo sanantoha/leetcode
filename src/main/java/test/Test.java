8b3f2b97ad4b98b594635239421a01e8ea6ea5ce
sanantoha <sanantoha@gmail.com>
2021-12-28 18:44:18 +0400

    +1 task. 28.12.2021.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            DFSTreeTraverseRec
-            AStarAlgo
             BreadthSearchFirst
             HouseRobber
             PrimMinSpanningTree
@@ -102,6 +100,8 @@ public class Test {
             MergeIntervals
             RiverSizes
             CountingSort
+            DFSTreeTraverseRec
+            AStarAlgo
          */
     }
 }
