769609b7003555959ee084841763b0acd94a3042
sanantoha <sanantoha@gmail.com>
2022-01-04 00:09:53 +0400

    Add WaterArea. 03.01.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -8,6 +8,7 @@ public class Test {
             SubarraySort
             LongestCommonSubsequence
             DepthFirstSearch
+            WaterArea
             SortList
             BinaryTreeZigzagLevelOrderTraverse
             Sqrt
