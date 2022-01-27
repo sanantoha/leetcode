885bfe1b3a396e026ad7bb44a007aa6ac1a25632
sanantoha <sanantoha@gmail.com>
2022-01-27 10:43:10 +0400

    +2 task. 27.01.2022.

--- src/main/java/list/PalindromeLinkedList5.java
+++ src/main/java/list/PalindromeLinkedList5.java
@@ -2,8 +2,41 @@ package list;
 
 public class PalindromeLinkedList5 {
 
+    // O(n) time | O(1) space
     public static boolean isPalindrome(ListNode head) {
-        return false;
+        if (head == null) return true;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode fst = head;
+        ListNode snd = reverse(slow);
+
+        while (snd != null) {
+            if (fst.val != snd.val) return false;
+            fst = fst.next;
+            snd = snd.next;
+        }
+
+        return true;
+    }
+
+    private static ListNode reverse(ListNode head) {
+        ListNode next;
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 
     public static void main(String[] args) {
--- src/main/java/tree/FindClosestValueInBST4.java
+++ src/main/java/tree/FindClosestValueInBST4.java
@@ -2,12 +2,46 @@ package tree;
 
 public class FindClosestValueInBST4 {
 
+    // O(h) time | O(1) space
     public static int findClosestValueInBst(TreeNode root, int target) {
-        return -1;
+        if (root == null) return -1;
+
+        TreeNode curr = root;
+        int closest = curr.val;
+
+        while (curr != null) {
+            if (Math.abs(curr.val - target) < Math.abs(closest - target)) {
+                closest = curr.val;
+            }
+
+            if (target > curr.val) {
+                curr = curr.right;
+            } else if (target < curr.val) {
+                curr = curr.left;
+            } else return curr.val;
+        }
+
+        return closest;
     }
 
+    // O(h) time | O(h) space
     public static int findClosestValueInBstRec(TreeNode root, int target) {
-        return -1;
+        if (root == null) return -1;
+        return findClosestValueInBstRec(root, root.val, target);
+    }
+
+    private static int findClosestValueInBstRec(TreeNode root, int closest, int target) {
+        if (root == null) return closest;
+
+        if (Math.abs(root.val - target) < Math.abs(closest - target)) {
+            closest = root.val;
+        }
+
+        if (target > root.val) {
+            return findClosestValueInBstRec(root.right, closest, target);
+        } else if (target < root.val) {
+            return findClosestValueInBstRec(root.left, closest, target);
+        } return root.val;
     }
 
     public static void main(String[] args) {
