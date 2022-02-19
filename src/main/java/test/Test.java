42d6073134bc0e5be6458b477676c7fb70199ec4
sanantoha <sanantoha@gmail.com>
2022-02-20 01:33:31 +0400

    +2 tasks. 19.02.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            SurroundedRegions
-            SpiralMatrixTraverse
             ProductOfArrayExceptSelf
             LevenshteinDistance
             DeleteNodeInLinkedList
@@ -107,6 +105,8 @@ public class Test {
             ReconstructBST
             LeftViewBinaryTree
             SearchInRotatedSortedArray
+            SurroundedRegions
+            SpiralMatrixTraverse
          */
     }
 }
--- src/main/java/tree/SurroundedRegions4.java
+++ src/main/java/tree/SurroundedRegions4.java
@@ -1,11 +1,52 @@
 package tree;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class SurroundedRegions4 {
 
+    // O(w * h) time | O(1) space
     public static void solve(char[][] board) {
+        if (board == null || board.length == 0) return;
 
+        for (int i = 0; i < board.length; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, board[0].length - 1);
+        }
+
+        for (int i = 0; i < board[0].length; i++) {
+            dfs(board, 0, i);
+            dfs(board, board.length - 1, i);
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == '*') board[i][j] = 'O';
+                else if (board[i][j] == 'O') board[i][j] = 'X';
+            }
+        }
+    }
+
+    private static void dfs(char[][] board, int row, int col) {
+        if (board[row][col] != 'O') return;
+
+        board[row][col] = '*';
+
+        for (int[] pair : getNeighbors(board, row, col)) {
+            int rowFut = pair[0];
+            int colFut = pair[1];
+            dfs(board, rowFut, colFut);
+        }
+    }
+
+    private static List<int[]> getNeighbors(char[][] board, int row, int col) {
+        List<int[]> neighbors = new ArrayList<>();
+        if (row > 0) neighbors.add(new int[] {row - 1, col});
+        if (col > 0) neighbors.add(new int[] {row, col - 1});
+        if (row + 1 < board.length) neighbors.add(new int[] {row + 1, col});
+        if (col + 1 < board[row].length) neighbors.add(new int[] {row, col + 1});
+        return neighbors;
     }
 
     public static void main(String[] args) {
