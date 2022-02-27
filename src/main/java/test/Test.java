6c5ba785d0bb1d369f847079157ec784ef816c7f
sanantoha <sanantoha@gmail.com>
2022-02-28 01:31:15 +0400

    Add one task 27.02.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -16,6 +16,7 @@ public class Test {
             ValidIPAddress
             PopulatingNextRightPointerInEachNode
             SubarraySort
+            DiskStacking
             MedianOfTwoSortedArrays
             DepthFirstSearch
             SortList
