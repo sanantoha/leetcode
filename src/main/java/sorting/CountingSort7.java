ab1c29f375ec3e44ff647380f651c65987c0a3b1
sanantoha <sanantoha@gmail.com>
2022-02-28 13:02:21 +0400

    Add one task 28.02.2022.

--- src/main/java/sorting/CountingSort7.java
+++ src/main/java/sorting/CountingSort7.java
@@ -5,8 +5,29 @@ import java.util.Random;
 
 public class CountingSort7 {
 
+    // O(n) time | O(k) space - where k unique elements
     public static void countingSort(int[] arr) {
+        if (arr == null || arr.length == 0) return;
 
+        int minVal = arr[0];
+        int maxVal = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            minVal = Math.min(minVal, arr[i]);
+            maxVal = Math.max(maxVal, arr[i]);
+        }
+
+        int[] cnt = new int[maxVal - minVal + 1];
+        for (int i = 0; i < arr.length; i++) {
+            cnt[arr[i] - minVal]++;
+        }
+
+        int idx = 0;
+        for (int i = 0; i < cnt.length; i++) {
+            for (int j = 0; j < cnt[i]; j++) {
+                arr[idx++] = i + minVal;
+            }
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            RiverSizes
-            CountingSort
             DFSTreeTraverseRec
             AStarAlgo
             BreadthSearchFirst
@@ -108,6 +106,8 @@ public class Test {
             QuickSort
             AddTwoNumbers
             MergeIntervals
+            RiverSizes
+            CountingSort
          */
     }
 }
