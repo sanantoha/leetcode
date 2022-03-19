2349c522cea63e4374441255561dd9e66f5071f2
sanantoha <sanantoha@gmail.com>
2022-03-19 15:36:26 +0400

    +2 19.03.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            LongestCommonSubsequence
-            LowestCommonAncestorOfBinarySearchTree
             LongestIncrSubSeq
             ValidateBinarySearchTree
             AStarAlgo
@@ -109,6 +107,8 @@ public class Test {
             BestTimeToBuyAndSellStock
             BinarySearch
             WaterArea
+            LongestCommonSubsequence
+            LowestCommonAncestorOfBinarySearchTree
          */
     }
 }
--- src/main/java/tree/LowestCommonAncestorOfBinarySearchTree10.java
+++ src/main/java/tree/LowestCommonAncestorOfBinarySearchTree10.java
@@ -1,17 +1,9 @@
 package tree;
 
-public class LowestCommonAncestorOfBinarySearchTree9 {
+public class LowestCommonAncestorOfBinarySearchTree10 {
 
-    // O(h) time | O(h) space
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null;
-
-        if (p.val < root.val && q.val < root.val) {
-            return lowestCommonAncestor(root.left, p, q);
-        } else if (p.val > root.val && q.val > root.val) {
-            return lowestCommonAncestor(root.right, p, q);
-        }
-        return root;
+        return null;
     }
 
     public static void main(String[] args) {
