170846ade2b244eb39b4e9eb4b707bdaaedc5bd4
sanantoha <sanantoha@gmail.com>
2022-04-05 11:50:12 +0400

    add 1 task 05.04.2022.

---
+++ src/main/java/array/SparseMatrixMultiplication.java
@@ -0,0 +1,42 @@
+package array;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/explore/interview/card/apple/344/array-and-strings/2040/
+ */
+public class SparseMatrixMultiplication {
+
+    public static int[][] multiply(int[][] mat1, int[][] mat2) {
+        return new int[][] {
+                {7, 0, 0},
+                {-7, 0, 3}
+        };
+    }
+
+    public static int[][] multiply1(int[][] mat1, int[][] mat2) {
+        return new int[][] {
+                {7, 0, 0},
+                {-7, 0, 3}
+        };
+    }
+
+    public static void main(String[] args) {
+//        Input: mat1 = [[1,0,0],[-1,0,3]], mat2 = [[7,0,0],[0,0,0],[0,0,1]]
+//        Output: [[7,0,0],[-7,0,3]]
+
+        int[][] mat1 = {
+            {1, 0, 0},
+            {-1, 0, 3}
+        };
+        int[][] mat2 = {
+            {7, 0, 0},
+            {0, 0, 0},
+            {0, 0, 1}
+        };
+
+        System.out.println(Arrays.deepToString(multiply(mat1, mat2)));
+        System.out.println();
+        System.out.println(Arrays.deepToString(multiply1(mat1, mat2)));
+    }
+}
