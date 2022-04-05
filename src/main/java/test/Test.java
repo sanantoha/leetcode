bbee188fed5c72475c73eb5c096dd2a0a3e22138
sanantoha <sanantoha@gmail.com>
2022-04-05 11:50:47 +0400

    add 1 task 05.04.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -11,6 +11,7 @@ public class Test {
             KruskalMinSpanningTree
             ReverseInteger
             TopologicalSortDFSCycleGraphAsMap
+            SparseMatrixMultiplication
             DijkstraShortestPaths
             PalindromicSubstrings
             RotateImage
