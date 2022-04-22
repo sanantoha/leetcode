7279d0c4965fff1034c108c28336882fbe102ff8
sanantoha <sanantoha@gmail.com>
2022-04-22 10:32:10 +0400

    +3 tasks 22.04.2022.

---
+++ src/main/java/backtracking/Powerset12.java
@@ -0,0 +1,20 @@
+package backtracking;
+
+import java.util.List;
+
+public class Powerset12 {
+
+    public static List<List<Integer>> powerset(int[] arr) {
+        return null;
+    }
+
+    public static List<List<Integer>> powersetRec(int[] arr) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        System.out.println(powerset(arr));
+        System.out.println(powersetRec(arr));
+    }
+}
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,9 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            Powerset
-            AddTwoNumbers
-            MergeIntervals
             RiverSizes
             Sqrt
             CountingSort
@@ -112,6 +109,9 @@ public class Test {
             MinHeap
             MinNumberOfCoinsForChange
             QuickSort
+            Powerset
+            AddTwoNumbers
+            MergeIntervals
          */
     }
 }
