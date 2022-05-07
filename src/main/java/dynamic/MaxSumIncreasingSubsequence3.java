aa338ac5722edb3e4747af5e63065e49409307d5
sanantoha <sanantoha@gmail.com>
2022-05-07 14:00:36 +0400

    +3 tasks 07.05.2022.

--- src/main/java/dynamic/MaxSumIncreasingSubsequence3.java
+++ src/main/java/dynamic/MaxSumIncreasingSubsequence3.java
@@ -1,5 +1,8 @@
 package dynamic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -7,15 +10,56 @@ import java.util.List;
  */
 public class MaxSumIncreasingSubsequence3 {
 
+    // O(n ^ 2) time | O(n) space
     public static List<List<Integer>> maxSumIncreasingSubsequence(int[] array) {
+        if (array == null || array.length == 0) return Collections.emptyList();
 
-        return List.of(List.of(110), List.of(10, 20, 30, 50));
+        int[] msi = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            msi[i] = array[i];
+        }
+        int[] prev = new int[array.length];
+        Arrays.fill(prev, -1);
+
+        int maxVal = Integer.MIN_VALUE;
+        int maxIdx = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] < array[i] && msi[i] < msi[j] + array[i]) {
+                    msi[i] = msi[j] + array[i];
+                    prev[i] = j;
+                }
+            }
+            if (maxVal < msi[i]) {
+                maxVal = msi[i];
+                maxIdx = i;
+            }
+        }
+
+
+        return List.of(List.of(maxVal), buildList(array, prev, maxIdx));
+    }
+
+    private static List<Integer> buildList(int[] arr, int[] prev, int maxIdx) {
+        List<Integer> res = new ArrayList<>();
+        int idx = maxIdx;
+
+        while (idx >= 0) {
+            res.add(arr[idx]);
+            idx = prev[idx];
+        }
+
+        Collections.reverse(res);
+        return res;
     }
 
     public static void main(String[] args) {
         int[] input = {10, 70, 20, 30, 50, 11, 30};
         int[][] expected = {{110}, {10, 20, 30, 50}};
-        System.out.println(compare(maxSumIncreasingSubsequence(input), expected));
+        List<List<Integer>> actual = maxSumIncreasingSubsequence(input);
+        System.out.println(actual);
+        System.out.println(compare(actual, expected));
     }
 
     public static boolean compare(List<List<Integer>> arr1, int[][] arr2) {
---
+++ src/main/java/dynamic/MaxSumIncreasingSubsequence4.java
@@ -0,0 +1,32 @@
+package dynamic;
+
+import java.util.List;
+
+/**
+ * https://www.algoexpert.io/questions/Max%20Sum%20Increasing%20Subsequence
+ */
+public class MaxSumIncreasingSubsequence4 {
+
+    public static List<List<Integer>> maxSumIncreasingSubsequence(int[] array) {
+
+        return List.of(List.of(110), List.of(10, 20, 30, 50));
+    }
+
+    public static void main(String[] args) {
+        int[] input = {10, 70, 20, 30, 50, 11, 30};
+        int[][] expected = {{110}, {10, 20, 30, 50}};
+        System.out.println(compare(maxSumIncreasingSubsequence(input), expected));
+    }
+
+    public static boolean compare(List<List<Integer>> arr1, int[][] arr2) {
+        if (arr1.get(0).get(0) != arr2[0][0]) {
+            return false;
+        }
+        for (int i = 0; i < arr1.get(1).size(); i++) {
+            if (arr1.get(1).get(i) != arr2[1][i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
