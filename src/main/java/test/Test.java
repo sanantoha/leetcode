3afc46bef3b38e5ebd55379740dba9def4ab3c2b
sanantoha <sanantoha@gmail.com>
2022-05-08 15:10:37 +0400

    +3 tasks 08.05.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            BellmanFord
-            BranchSums
             FirstPermutationIsSubstrSecondStr
             GenerateParentheses
             PrimMinSpanningTree
@@ -113,6 +111,8 @@ public class Test {
             FindNodesDistanceK
             LongestDecrSubseq
             MaxSumIncreasingSubsequence
+            BellmanFord
+            BranchSums
          */
     }
 }
