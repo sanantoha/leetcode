9528edc3f5ef5b07ec06cfd4c15d84cc1f7805d2
sanantoha <sanantoha@gmail.com>
2022-05-28 13:54:00 +0400

    +2 tasks 28.05.2022.

--- src/main/java/sorting/SortKSortedArray1.java
+++ src/main/java/sorting/SortKSortedArray1.java
@@ -1,33 +1,11 @@
 package sorting;
 
 import java.util.Arrays;
-import java.util.PriorityQueue;
 
 public class SortKSortedArray1 {
 
-    // O(N * log(k)) time | O(k) space
     public static int[] sortKSortedArray(int[] arr, int k) {
-        if (arr == null || arr.length == 0) return arr;
-
-        PriorityQueue<Integer> heap = new PriorityQueue<>(k + 1);
-
-        for (int i = 0; i < Math.min(k + 1, arr.length); i++) {
-            heap.add(arr[i]);
-        }
-
-        int idx = 0;
-        for (int i = k + 1; i < arr.length; i++) {
-            int min = heap.remove();
-            arr[idx++] = min;
-
-            heap.add(arr[i]);
-        }
-
-        while (!heap.isEmpty()) {
-            arr[idx++] = heap.remove();
-        }
-
-        return arr;
+        return null;
     }
 
     public static void main(String[] args) {
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            StaircaseTraversal
-            SortKSortedArray
             KruskalMinSpanningTree
             ReverseInteger
             TopologicalSortDFSCycleGraphAsMap
@@ -120,6 +118,8 @@ public class Test {
             ClimbingStairs
             NextGreaterElement
             ConvertedSortedArrayToBST
+            StaircaseTraversal
+            SortKSortedArray
          */
     }
 }
