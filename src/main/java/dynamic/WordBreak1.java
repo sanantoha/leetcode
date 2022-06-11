61bc57bd7b6db80bf9c30746542f66b0c5611474
sanantoha <sanantoha@gmail.com>
2022-06-11 11:39:19 +0400

    +2 tasks 11.06.2022.

--- src/main/java/dynamic/WordBreak1.java
+++ src/main/java/dynamic/WordBreak1.java
@@ -1,6 +1,8 @@
 package dynamic;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WordBreak1 {
 
--- src/main/java/list/IntersectionLinkedList1.java
+++ src/main/java/list/IntersectionLinkedList1.java
@@ -1,56 +1,25 @@
 package list;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class IntersectionLinkedList1 {
 
-    public static ListNode intersection(ListNode left, ListNode right) {
-        if (left == null || right == null) return null;
-
-        Set<ListNode> set = new HashSet<>();
-
-        ListNode currL = left;
-        while (currL != null) {
-            set.add(currL);
-            currL = currL.next;
-        }
-
-        ListNode currR = right;
-        while (currR != null) {
-            if (set.contains(currR)) return currR;
-            currR = currR.next;
-        }
-
+    public static ListNode getIntersectionNode(ListNode l, ListNode r) {
         return null;
     }
 
-    public static ListNode intersectionWS(ListNode left, ListNode right) {
-        if (left == null || right == null) return null;
-
-        ListNode currL = left;
-        ListNode currR = right;
-
-        while (currL != currR) {
-            currL = (currL == null) ? right : currL.next;
-            currR = (currR == null) ? left : currR.next;
-        }
-        return currL;
+    public static ListNode getIntersectionNode1(ListNode l, ListNode r) {
+        return null;
     }
 
     public static void main(String[] args) {
+        ListNode common = new ListNode(8, new ListNode(4, new ListNode(5)));
+        ListNode l1 = new ListNode(4, new ListNode(1, common));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(1, common)));
 
-        ListNode inter = new ListNode(5, new ListNode(6, new ListNode(7)));
-        ListNode rootL = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, inter))));
-        ListNode rootR = new ListNode(-2, new ListNode(-1, new ListNode(0, inter)));
-
-        ListNode rootR1 = new ListNode(100, new ListNode(200));
-
-
-        System.out.println(intersection(rootL, rootR));
-        System.out.println(intersection(rootL, rootR1));
-
-        System.out.println(intersectionWS(rootL, rootR));
-        System.out.println(intersectionWS(rootL, rootR1));
+        System.out.println(getIntersectionNode(l1, l2));
+        System.out.println(getIntersectionNode1(l1, l2));
     }
 }
