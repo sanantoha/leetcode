d982cdc004737c66ee460e4cffc3ca6682488c1e
sanantoha <sanantoha@gmail.com>
2022-06-17 14:14:31 +0400

    +2 tasks 17.06.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            MergeIntervals
-            RiverSizes
             Sqrt
             CountingSort
             DFSTreeTraverseRec
@@ -121,6 +119,8 @@ public class Test {
             QuickSort
             Powerset
             AddTwoNumbers
+            MergeIntervals
+            RiverSizes
          */
     }
 }
