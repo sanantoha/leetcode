3c387b8e5bf6039ccaeedfaff9138c66511dddbe
sanantoha <sanantoha@gmail.com>
2022-06-17 13:54:27 +0400

    +2 tasks 17.06.2022.

--- src/main/java/sorting/MergeIntervals1.java
+++ src/main/java/sorting/MergeIntervals1.java
@@ -1,26 +1,11 @@
 package sorting;
 
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedList;
 
 public class MergeIntervals1 {
 
     public static int[][] merge(int[][] intervals) {
-        if (intervals == null || intervals.length == 0) return new int[][] {};
-
-        Arrays.sort(intervals, Comparator.comparingInt(x -> x[0]));
-
-        LinkedList<int[]> merged = new LinkedList<>();
-
-        for (int[] interval : intervals) {
-            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
-                merged.add(interval);
-            } else {
-                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
-            }
-        }
-        return merged.toArray(new int[][] {});
+        return null;
     }
 
     public static void main(String[] args) {
