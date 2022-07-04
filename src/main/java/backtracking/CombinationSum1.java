8f53e6e34fd92d189c4a05f09e1c4a6b9357ca01
sanantoha <sanantoha@gmail.com>
2022-07-04 13:32:32 +0400

    +2 tasks 04.07.2022.

--- src/main/java/backtracking/CombinationSum1.java
+++ src/main/java/backtracking/CombinationSum1.java
@@ -1,31 +1,11 @@
 package backtracking;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CombinationSum1 {
 
-    // O(N ^ (M / T - 1)) time | O(T / M) space -
-    // where N number of candidates, T is the target value, M - the smallest candidate among all given integers
     public static List<List<Integer>> combinationSum(int[] arr, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (arr == null || arr.length == 0) return res;
-        backtrack(arr, target, 0, new ArrayList<>(), res);
-        return res;
-    }
-
-    private static void backtrack(int[] arr, int target, int idx, List<Integer> ans, List<List<Integer>> res) {
-        if (target < 0) return;
-        if (target == 0) {
-            res.add(new ArrayList<>(ans));
-            return;
-        }
-
-        for (int i = idx; i < arr.length; i++) {
-            ans.add(arr[i]);
-            backtrack(arr, target - arr[i], i, ans, res);
-            ans.remove(ans.size() - 1);
-        }
+        return null;
     }
 
     public static void main(String[] args) {
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            WaterArea
-            CombinationSum
             LongestCommonSubsequence
             LowestCommonAncestorOfBinarySearchTree
             LongestIncrSubSeq
@@ -121,6 +119,8 @@ public class Test {
             KruskalMinSpanningTree
             SameBSTs
             BSTSuccessorSearch
+            WaterArea
+            CombinationSum
          */
     }
 }
