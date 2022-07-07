55cbc1f3984f923c321ae558451cb31b62a04795
sanantoha <sanantoha@gmail.com>
2022-07-07 10:53:57 +0400

    +2 tasks 07.07.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            AStarAlgo
-            SameTree
             SearchForRange
             SparseMatrixMultiplication
             MinimumPassesOfMatrix
@@ -121,6 +119,8 @@ public class Test {
             LowestCommonAncestorOfBinarySearchTree
             LongestIncrSubSeq
             ValidateBinarySearchTree
+            AStarAlgo
+            SameTree
          */
     }
 }
