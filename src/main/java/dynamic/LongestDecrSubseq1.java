0d026a2a16a37c9b51aa707351afc624a77e6513
sanantoha <sanantoha@gmail.com>
2022-07-10 15:11:30 +0400

    +2 tasks 10.07.2022.

--- src/main/java/dynamic/LongestDecrSubseq1.java
+++ src/main/java/dynamic/LongestDecrSubseq1.java
@@ -1,151 +1,34 @@
 package dynamic;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class LongestDecrSubseq1 {
 
-    // O(n ^ 2) time | O(n) space
     public static int lds(int[] arr) {
-        if (arr == null || arr.length == 0) return 0;
-
-        int[] lds = new int[arr.length];
-        Arrays.fill(lds, 1);
-        int longest = 0;
-
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[i] && lds[i] < lds[j] + 1) {
-                    lds[i] = lds[j] + 1;
-                }
-            }
-            if (longest < lds[i]) {
-                longest = lds[i];
-            }
-        }
-
-        return longest;
+        return -1;
     }
 
-    // O(n ^ 2) time | O(n) space
     public static List<Integer> ldsList(int[] arr) {
-        if (arr == null || arr.length == 0) return Collections.emptyList();
-
-        int[] lds = new int[arr.length];
-        Arrays.fill(lds, 1);
-        int[] prev = new int[arr.length];
-        Arrays.fill(prev, -1);
-
-        int longest = 0;
-        int maxIdx = 0;
-
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[i] && lds[i] < lds[j] + 1) {
-                    lds[i] = lds[j] + 1;
-                    prev[i] = j;
-                }
-            }
-            if (longest < lds[i]) {
-                longest = lds[i];
-                maxIdx = i;
-            }
-        }
-        return buildList(arr, prev, maxIdx);
+        return null;
     }
 
-    private static List<Integer> buildList(int[] arr, int[] prev, int maxIdx) {
-        List<Integer> res = new ArrayList<>();
-        int idx = maxIdx;
-        while (idx >= 0) {
-            res.add(arr[idx]);
-            idx = prev[idx];
-        }
-        Collections.reverse(res);
-        return res;
-    }
-
-    // O(n * log(n)) time | O(n) space
     public static int lds1(int[] arr) {
-        if (arr == null || arr.length == 0) return 0;
-
-        List<Integer> res = new ArrayList<>();
-        res.add(arr[0]);
-
-        for (int i = 1; i < arr.length; i++) {
-            int prev = res.get(res.size() - 1);
-            if (prev > arr[i]) {
-                res.add(arr[i]);
-            } else {
-                int j = binarySearch(res, arr[i]);
-                if (j < 0) {
-                    j = -(j + 1);
-                }
-                res.set(j, arr[i]);
-            }
-        }
-        return res.size();
+        return -1;
     }
 
-    private static int binarySearch(List<Integer> lst, int target) {
-        int l = 0;
-        int r = lst.size() - 1;
-
-        while (l <= r) {
-            int mid = (l + r) >>> 1;
-            if (target <= lst.get(mid)) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-
-        return lst.get(l) == target ? l : -(l + 1);
-    }
-
-    // O(n * log(n)) time | O(n) space
     public static List<Integer> ldsList1(int[] arr) {
-        if (arr == null || arr.length == 0) return Collections.emptyList();
-
-        int[] indices = new int[arr.length + 1];
-        Arrays.fill(indices, -1);
-        int[] prev = new int[arr.length];
-        Arrays.fill(prev, -1);
-
-        int length = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            int newLength = binarySearch(arr, indices, 1, length, arr[i]);
-            prev[i] = indices[newLength - 1];
-            indices[newLength] = i;
-            length = Math.max(length, newLength);
-        }
-        return buildList(arr, prev, indices[length]);
-    }
-
-    private static int binarySearch(int[] arr, int[] indices, int l, int r, int target) {
-        while (l <= r) {
-            int mid = (l + r) >>> 1;
-            if (target > arr[indices[mid]]) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return l;
+        return null;
     }
 
     public static void main(String[] args) {
-        int[] arr = {5,6,7,6,5,4,3,10,14,12};
+        int[] arr = {5,6,7,6,5,4,3,10,14,12}; // 5
 
         System.out.println(lds(arr));
         System.out.println(lds1(arr));
         System.out.println(ldsList(arr));
         System.out.println(ldsList1(arr));
 
-        int[] arr1 = {100, 10, 9, 8, 7, 6, 5, 90, 80, 70, 60, 50, 40, 30, 20};
+        int[] arr1 = {100, 10, 9, 8, 7, 6, 5, 90, 80, 70, 60, 50, 40, 30, 20}; // 9
         System.out.println(lds(arr1));
         System.out.println(lds1(arr1));
         System.out.println(ldsList(arr1));
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            FindNodesDistanceK
-            LongestDecrSubseq
             MaxSumIncreasingSubsequence
             BellmanFord
             BranchSums
@@ -121,6 +119,8 @@ public class Test {
             SearchForRange
             MinimumPassesOfMatrix
             HeapSort
+            FindNodesDistanceK
+            LongestDecrSubseq
          */
     }
 }
