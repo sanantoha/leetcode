a4e99cb651164bf3ed070758a76b6c72dd08b754
sanantoha <sanantoha@gmail.com>
2022-07-14 10:59:59 +0400

    +2 tasks 14.07.2022.

--- src/main/java/array/ZigZagTraverse1.java
+++ src/main/java/array/ZigZagTraverse1.java
@@ -6,53 +6,9 @@ import java.util.List;
 
 public class ZigZagTraverse1 {
 
-    // O(n) time | O(n) space
     public static List<Integer> zigzagTraverse(List<List<Integer>> array) {
-        List<Integer> res = new ArrayList<>();
-        if (array == null || array.size() == 0) return res;
-
-        int row = 0;
-        int col = 0;
-        int height = array.size() - 1;
-        int width = array.get(0).size() - 1;
-
-        boolean isGoingDown = true;
-
-        while (!isOutOfBound(row, col, height, width)) {
-            res.add(array.get(row).get(col));
-            if (isGoingDown) {
-                if (col == 0 || row == height) {
-                    isGoingDown = false;
-                    if (row == height) {
-                        col++;
-                    } else {
-                        row++;
-                    }
-                } else {
-                    row++;
-                    col--;
-                }
-            } else {
-                if (row == 0 || col == width) {
-                    isGoingDown = true;
-                    if (col == width) {
-                        row++;
-                    } else {
-                        col++;
-                    }
-                } else {
-                    row--;
-                    col++;
-                }
-            }
-        }
-        return res;
+        return null;
     }
-
-    private static boolean isOutOfBound(int row, int col, int height, int width) {
-        return row < 0 || col < 0 || row > height || col > width;
-    }
-
 
     public static void main(String[] args) {
         List<List<Integer>> test = new ArrayList<List<Integer>>();
--- src/main/java/list/PalindromeLinkedList1.java
+++ src/main/java/list/PalindromeLinkedList1.java
@@ -3,36 +3,11 @@ package list;
 public class PalindromeLinkedList1 {
 
     public static boolean isPalindrome(ListNode head) {
-
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode prev = null;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-
-            ListNode next = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = next;
-        }
-
-        if (fast != null) {
-            slow = slow.next;
-        }
-
-        ListNode c1 = prev;
-        ListNode c2 = slow;
-        while (c1 != null && c2 != null) {
-            if (c1.val != c2.val) return false;
-            c1 = c1.next;
-            c2 = c2.next;
-        }
-
-        return true;
+        return false;
     }
 
     public static void main(String[] args) {
-        ListNode lst = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(2, new ListNode(1)))));
+        ListNode lst = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3, new ListNode(2, new ListNode(1))))));
         ListNode lst1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(2, new ListNode(1)))));
 
         System.out.println(isPalindrome(lst));
