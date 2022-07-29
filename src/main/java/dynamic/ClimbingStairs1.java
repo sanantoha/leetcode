80fa35df2da3f15412641faa656d8e092245123f
sanantoha <sanantoha@gmail.com>
2022-07-29 12:13:22 +0400

    +2 tasks 29.07.2022.

--- src/main/java/dynamic/ClimbingStairs1.java
+++ src/main/java/dynamic/ClimbingStairs1.java
@@ -7,53 +7,29 @@ package dynamic;
  */
 public class ClimbingStairs1 {
 
-    // O(n) time | O(n) space
     public static int climbStairsDP(int n) {
-        if (n <= 0) return 0;
-        if (n == 1) return 1;
-
-        int[] ways = new int[n + 1];
-        ways[0] = 0;
-        ways[1] = 1;
-        ways[2] = 2;
-
-        for (int i = 3; i <= n; i++) {
-            ways[i] = ways[i - 2] + ways[i - 1];
-        }
-
-        return ways[n];
+        return -1;
     }
 
-    // O(n) time | O(1)
     public static int climbStairs(int n) {
-        if (n <= 0) return 0;
-        if (n == 1) return 1;
-
-        int fst = 1;
-        int snd = 2;
-        for (int i = 3; i <= n; i++) {
-            int tmp = fst + snd;
-            fst = snd;
-            snd = tmp;
-        }
-        return snd;
+        return -1;
     }
 
     public static void main(String[] args) {
-        System.out.println(climbStairsDP(0));
-        System.out.println(climbStairsDP(1));
-        System.out.println(climbStairsDP(2));
-        System.out.println(climbStairsDP(3));
-        System.out.println(climbStairsDP(4));
-        System.out.println(climbStairsDP(5));
+        System.out.println(climbStairsDP(0) == 0);
+        System.out.println(climbStairsDP(1) == 1);
+        System.out.println(climbStairsDP(2) == 2);
+        System.out.println(climbStairsDP(3) == 3);
+        System.out.println(climbStairsDP(4) == 5);
+        System.out.println(climbStairsDP(5) == 8);
 
         System.out.println("=======================");
 
-        System.out.println(climbStairs(0));
-        System.out.println(climbStairs(1));
-        System.out.println(climbStairs(2));
-        System.out.println(climbStairs(3));
-        System.out.println(climbStairs(4));
-        System.out.println(climbStairs(5));
+        System.out.println(climbStairs(0) == 0);
+        System.out.println(climbStairs(1) == 1);
+        System.out.println(climbStairs(2) == 2);
+        System.out.println(climbStairs(3) == 3);
+        System.out.println(climbStairs(4) == 5);
+        System.out.println(climbStairs(5) == 8);
     }
 }
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            ClimbingStairs
-            NextGreaterElement
             ConvertedSortedArrayToBST
             StaircaseTraversal
             SortKSortedArray
@@ -125,6 +123,8 @@ public class Test {
             SubarraySumEqualsK
             NumberOfWaysToMakeChange
             WordSearch
+            ClimbingStairs
+            NextGreaterElement
          */
     }
 }
