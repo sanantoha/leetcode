015922ddad14e5fb1cb89426f25e5687a48a94d7
sanantoha <sanantoha@gmail.com>
2022-08-02 12:46:25 +0400

    +2 tasks 02.08.2022.

--- src/main/java/graph/DijkstraShortestPaths1.java
+++ src/main/java/graph/DijkstraShortestPaths1.java
@@ -1,21 +1,9 @@
 package graph;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Scanner;
-
 public class DijkstraShortestPaths1 {
 
     // ShortestPath{shortest=[0.0, 5.0, 8.0, 4.0, 7.0], prev=[-1, 3, 1, 0, 3]}
     public static void main(String[] args) {
         String path = "src/main/java/graph/dijkstraShortestPath.txt";
-    }
-
-    public static ShortestPath findShortestPath(EdgeWeightedDigraph graph, int start) {
-        return null;
     }
 }
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            DijkstraShortestPaths
-            PalindromicSubstrings
             RotateImage
             SortList
             Pow
@@ -126,6 +124,8 @@ public class Test {
             ReverseInteger
             TopologicalSortDFSCycleGraphAsMap
             FirstDuplicateValue
+            DijkstraShortestPaths
+            PalindromicSubstrings
          */
     }
 }
