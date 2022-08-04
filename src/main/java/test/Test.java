1bf56dc278bd7c9a0097d198ce136c2e7962f4cd
sanantoha <sanantoha@gmail.com>
2022-08-04 10:58:28 +0400

    +2 tasks 04.08.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            Pow
-            KnapsackProblem
             ReverseString
             RegularExpressionMatch
             BreadthSearchFirstAsMap
@@ -126,6 +124,8 @@ public class Test {
             PalindromicSubstrings
             RotateImage
             SortList
+            Pow
+            KnapsackProblem
          */
     }
 }
