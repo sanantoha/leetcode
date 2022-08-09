abc62ced538f5f1789caa17b798bc669192acd9e
sanantoha <sanantoha@gmail.com>
2022-08-09 08:19:48 +0400

    +2 tasks 09.08.2022.

--- src/main/java/backtracking/permutation/Permutations1.java
+++ src/main/java/backtracking/permutation/Permutations1.java
@@ -7,6 +7,7 @@ public class Permutations1 {
     private static List<List<Integer>> permute(int[] arr) {
         return null;
     }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3};
         System.out.println(permute(arr));
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            Permutations
-            LeftViewBinaryTree
             SearchInRotatedSortedArray
             MinimumWindowSubstring
             SurroundedRegions
@@ -126,6 +124,8 @@ public class Test {
             ThreeSum
             BalancedBrackets
             WordLadderII
+            Permutations
+            LeftViewBinaryTree
          */
     }
 }
