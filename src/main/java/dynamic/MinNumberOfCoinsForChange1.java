b2f11faac98f43ed36b56c8cd09413ecbc5d8ed8
sanantoha <sanantoha@gmail.com>
2022-08-18 00:28:12 +0400

    +2 tasks 17.08.2022.

--- src/main/java/dynamic/MinNumberOfCoinsForChange1.java
+++ src/main/java/dynamic/MinNumberOfCoinsForChange1.java
@@ -1,29 +1,10 @@
 package dynamic;
 
-import java.util.Arrays;
 
 public class MinNumberOfCoinsForChange1 {
 
-    // O(n * d) time | O(n) space
     public static int minNumberOfCoinsForChange(int n, int[] denoms) {
-        if (n < 0 || denoms == null || denoms.length == 0) return 0;
-
-        int[] minNums = new int[n + 1];
-        Arrays.fill(minNums, Integer.MAX_VALUE);
-        minNums[0] = 0;
-
-        for (int denom : denoms) {
-            for (int amount = 1; amount <= n; amount++) {
-                if (amount >= denom) {
-                    int toChange = minNums[amount - denom];
-                    if (toChange != Integer.MAX_VALUE) {
-                        toChange++;
-                    }
-                    minNums[amount] = Math.min(minNums[amount], toChange);
-                }
-            }
-        }
-        return minNums[n];
+        return -1;
     }
 
     public static void main(String[] args) {
