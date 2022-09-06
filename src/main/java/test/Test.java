a18c3718cb2e6f8982d1ce8a0b536d43fe2648b4
sanantoha <sanantoha@gmail.com>
2022-09-06 23:17:31 +0400

    +2 tasks 06.09.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -4,8 +4,8 @@ public class Test {
     public static void main(String[] args) {
         /*
             KruskalMinSpanningTree
-            SameBSTs
             BSTSuccessorSearch
+            SameBSTs
             WaterArea
             CombinationSum
             LongestCommonSubsequence
