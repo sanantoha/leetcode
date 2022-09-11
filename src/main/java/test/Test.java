f6c7b618dc2a5fa3ca93293480c6c7024dab4b17
sanantoha <sanantoha@gmail.com>
2022-09-11 14:25:59 +0400

    +2 tasks 11.09.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            ValidateBinarySearchTree
-            AStarAlgo
             SameTree
             RegularExpressionMatch
             SearchForRange
@@ -129,6 +127,8 @@ public class Test {
             LongestCommonSubsequence
             LowestCommonAncestorOfBinarySearchTree
             LongestIncrSubSeq
+            ValidateBinarySearchTree
+            AStarAlgo
          */
     }
 }
