7158053b9b6ac12533d59273528980070806ba52
sanantoha <sanantoha@gmail.com>
2022-09-16 12:01:51 +0400

    +2 tasks 16.09.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,7 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            BranchSums
             FirstPermutationIsSubstrSecondStr
             GenerateParentheses
             FindClosestValueInBST
@@ -129,6 +128,7 @@ public class Test {
             LongestDecrSubseq
             MaxSumIncreasingSubsequence
             BellmanFord
+            BranchSums
          */
     }
 }
