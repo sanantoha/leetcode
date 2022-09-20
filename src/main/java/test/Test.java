d99065635e1887ba7a0832218af50fe5688cc441
sanantoha <sanantoha@gmail.com>
2022-09-20 22:58:04 +0400

    +2 tasks 20.09.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            TopologicalSortDFSCycleGraph
-            ReconstructBST
             DijkstraShortestPathsAsMap
             MinimalHeaviestSetA
             PrimMinSpanningTreeAsMap
@@ -130,6 +128,8 @@ public class Test {
             ZigZagTraverse
             PalindromeLinkedList
             MaxPathSumInBinaryTree
+            TopologicalSortDFSCycleGraph
+            ReconstructBST
          */
     }
 }
