a45e436e74dcd4cba1945cea089c0efba0f8cc32
sanantoha <sanantoha@gmail.com>
2022-09-24 14:03:24 +0400

    +2 tasks 23.09.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            MinNumberOfJumps
-            SparseMatrixMultiplication
             LongestNonDecrSubseq
             StringPermutation
             DeepCopyArbitraryPointer
@@ -130,6 +128,8 @@ public class Test {
             MinimalHeaviestSetA
             PrimMinSpanningTreeAsMap
             SymmetricTree
+            MinNumberOfJumps
+            SparseMatrixMultiplication
          */
     }
 }
