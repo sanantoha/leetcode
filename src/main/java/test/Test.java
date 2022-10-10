f9f192b3d8d8e1a7115c7c01654c5f7f0dc83ad1
sanantoha <sanantoha@gmail.com>
2022-10-10 11:36:41 +0400

    +2 tasks 10.10.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            KnapsackProblem
-            ReverseString
             ConstructBinaryTreeFromPreorderAndInorderTraversal
             BreadthSearchFirstAsMap
             MaximumSubarray
@@ -130,6 +128,8 @@ public class Test {
             RotateImage
             SortList
             Pow
+            KnapsackProblem
+            ReverseString
          */
     }
 }
