380ce9cbfc2677c4f2c214acb75cd48c39bcfb1f
sanantoha <sanantoha@gmail.com>
2022-10-20 10:32:38 +0400

    +2 tasks 20.10.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            WordBreak
-            IntersectionLinkedList
             LRUCache
             MergeSort
             CloneGraph
@@ -130,6 +128,8 @@ public class Test {
             BellmanFordAsMap
             RemoveNthNodeFromEndOfList
             MinRewards
+            WordBreak
+            IntersectionLinkedList
          */
     }
 }
