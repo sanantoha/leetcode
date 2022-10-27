f87cf6c5bc0b2fc95e697ade9d6b8bc9b419eb8b
sanantoha <sanantoha@gmail.com>
2022-10-27 10:45:14 +0400

    +2 tasks 27.10.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            CountingSort
-            DFSTreeTraverseRec
             SubarraySort
             BreadthSearchFirst
             ValidateStartingCity
@@ -131,6 +129,8 @@ public class Test {
             RiverSizes
             Sqrt
             TopKFrequentWords
+            CountingSort
+            DFSTreeTraverseRec
          */
     }
 }
