dda9f87c62b274c296dec6243f57065da5e8d9de
sanantoha <sanantoha@gmail.com>
2022-11-01 22:32:04 +0400

    +2 tasks 1.11.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            UniquePaths
-            BinaryTreeDiameter
             ValidIPAddress
             PopulatingNextRightPointerInEachNode
             MedianOfTwoSortedArrays
@@ -131,6 +129,8 @@ public class Test {
             MinSwapsTransformStringToPalindrome
             StringWithoutAAAorBBB
             BFSTreeTraverse
+            UniquePaths
+            BinaryTreeDiameter
          */
     }
 }
