57157683da93a84a5d88c6069f59a7f9d3a49345
sanantoha <sanantoha@gmail.com>
2022-11-02 10:57:28 +0400

    +2 tasks 2.11.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            ValidIPAddress
-            PopulatingNextRightPointerInEachNode
             MedianOfTwoSortedArrays
             BinaryTreeZigzagLevelOrderTraverse
             DepthFirstSearch
@@ -131,6 +129,8 @@ public class Test {
             BFSTreeTraverse
             UniquePaths
             BinaryTreeDiameter
+            ValidIPAddress
+            PopulatingNextRightPointerInEachNode
          */
     }
 }
