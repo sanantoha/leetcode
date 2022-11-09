9e3a470d5ca269980ed1378ec22c8750743b477a
sanantoha <sanantoha@gmail.com>
2022-11-09 10:40:17 +0400

    +2 tasks 9.11.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            BinarySearch
-            PrimMinSpanningTree
             KruskalMinSpanningTree
             BSTSuccessorSearch
             SameBSTs
@@ -131,6 +129,8 @@ public class Test {
             BalancedBinaryTree
             KthSmallestElementInArray
             BestTimeToBuyAndSellStock
+            BinarySearch
+            PrimMinSpanningTree
          */
     }
 }
