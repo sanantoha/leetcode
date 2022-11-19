80d5ab8277b1b174aad7dd4ae853027c7241cc90
sanantoha <sanantoha@gmail.com>
2022-11-19 11:56:52 +0400

    +2 tasks 19.11.2022.

--- src/main/java/graph/BellmanFord1.java
+++ src/main/java/graph/BellmanFord1.java
@@ -3,7 +3,7 @@ package graph;
 public class BellmanFord1 {
 
     // ShortestPath{shortest=[-9.0, -20.0, -18.0, -2.0, -11.0], prev=[4, 2, 4, 0, 1]}
-    // [4, 1, 2]
+    // [1, 2, 4]
     public static void main(String[] args) {
         String path = "src/main/java/graph/bellmanFord.txt";
     }
