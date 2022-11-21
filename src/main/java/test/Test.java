95e788f1bb70bb030518cc6b96b6675804eaceca
sanantoha <sanantoha@gmail.com>
2022-11-21 12:58:55 +0400

    +2 tasks 21.11.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            FindClosestValueInBST
-            ZigZagTraverse
             PalindromeLinkedList
             MaxPathSumInBinaryTree
             TopologicalSortDFSCycleGraph
@@ -131,6 +129,8 @@ public class Test {
             BranchSums
             FirstPermutationIsSubstrSecondStr
             GenerateParentheses
+            FindClosestValueInBST
+            ZigZagTraverse
          */
     }
 }
