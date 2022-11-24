ad30f2e307067d25d8844b4d08d716e446de60b0
sanantoha <sanantoha@gmail.com>
2022-11-24 13:20:22 +0400

    +2 tasks 24.11.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            ReconstructBST
-            DijkstraShortestPathsAsMap
             MinimalHeaviestSetA
             PrimMinSpanningTreeAsMap
             SymmetricTree
@@ -132,6 +130,8 @@ public class Test {
             MaxPathSumInBinaryTree
             TopologicalSortDFSCycleGraph
             AllPathsFromSourceTarget
+            ReconstructBST
+            DijkstraShortestPathsAsMap
          */
     }
 }
