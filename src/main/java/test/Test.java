e7c454070b07e25c27aac14d941c40b56b8c28e6
sanantoha <sanantoha@gmail.com>
2022-11-30 15:12:10 +0400

    +2 tasks 30.11.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            DFSTreeTraverse
-            KruskalMinSpanningTreeAsMap
             CycleLinkedList
             DiskStacking
             LargestRange
@@ -132,6 +130,8 @@ public class Test {
             DeepCopyArbitraryPointer
             FirstUniqueCharacterInString
             RegularExpressionMatch
+            DFSTreeTraverse
+            KruskalMinSpanningTreeAsMap
          */
     }
 }
