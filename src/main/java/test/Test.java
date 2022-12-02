b7cbd352e7d255cf19e1152ec2a463f9170fd9fe
sanantoha <sanantoha@gmail.com>
2022-12-02 13:33:13 +0400

    +2 tasks 02.12.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            LargestRange
-            LowestCommonAncestorOfBinaryTree
             SelectSort
             DepthFirstSearchAsMap
             PhoneNumberMnemonic
@@ -132,6 +130,8 @@ public class Test {
             KruskalMinSpanningTreeAsMap
             CycleLinkedList
             DiskStacking
+            LargestRange
+            LowestCommonAncestorOfBinaryTree
          */
     }
 }
