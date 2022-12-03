1b6b1a240d6fa0fb00010206dbd98b878ad3393b
sanantoha <sanantoha@gmail.com>
2022-12-03 22:45:43 +0400

    +2 tasks 03.12.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            SelectSort
-            DepthFirstSearchAsMap
             PhoneNumberMnemonic
             FourSum
             SubarraySumEqualsK
@@ -132,6 +130,8 @@ public class Test {
             DiskStacking
             LargestRange
             LowestCommonAncestorOfBinaryTree
+            SelectSort
+            DepthFirstSearchAsMap
          */
     }
 }
