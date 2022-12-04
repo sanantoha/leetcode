586c15ef167f4a176be056805afcf648039f1638
sanantoha <sanantoha@gmail.com>
2022-12-04 14:33:33 +0400

    +2 tasks 04.12.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            PhoneNumberMnemonic
-            FourSum
             SubarraySumEqualsK
             NumberOfWaysToMakeChange
             WordSearch
@@ -132,6 +130,8 @@ public class Test {
             LowestCommonAncestorOfBinaryTree
             SelectSort
             DepthFirstSearchAsMap
+            PhoneNumberMnemonic
+            FourSum
          */
     }
 }
