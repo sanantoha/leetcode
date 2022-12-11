2f02080069683b7bea7cfba771ca6adb1a669ebb
sanantoha <sanantoha@gmail.com>
2022-12-11 14:12:30 +0400

    +2 tasks 11.12.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            DijkstraShortestPaths
-            PalindromicSubstrings
             RotateImage
             SortList
             Pow
@@ -132,6 +130,8 @@ public class Test {
             ReverseInteger
             TopologicalSortDFSCycleGraphAsMap
             FirstDuplicateValue
+            DijkstraShortestPaths
+            PalindromicSubstrings
          */
     }
 }
