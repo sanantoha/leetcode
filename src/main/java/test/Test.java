59697bd24beb9f88751537d639a97d816b06fd47
sanantoha <sanantoha@gmail.com>
2022-12-17 09:37:19 +0400

    +2 tasks 17.12.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            WordLadderII
-            Permutations
             LeftViewBinaryTree
             SearchInRotatedSortedArray
             SurroundedRegions
@@ -132,6 +130,8 @@ public class Test {
             MaximumSubarray
             ThreeSum
             BalancedBrackets
+            WordLadderII
+            Permutations
          */
     }
 }
