0632588522a47fe22b082d6ba2f60c92c4b0c6b2
sanantoha <sanantoha@gmail.com>
2022-12-18 13:44:03 +0400

    +2 tasks 18.12.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            LeftViewBinaryTree
-            SearchInRotatedSortedArray
             SurroundedRegions
             SpiralMatrixTraverse
             ProductOfArrayExceptSelf
@@ -132,6 +130,8 @@ public class Test {
             BalancedBrackets
             WordLadderII
             Permutations
+            LeftViewBinaryTree
+            SearchInRotatedSortedArray
          */
     }
 }
