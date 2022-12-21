c31b1e56f09e5b6f1deb6ae95d172da338827bac
sanantoha <sanantoha@gmail.com>
2022-12-21 10:34:10 +0400

    +2 tasks 21.12.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            DeleteNodeInLinkedList
-            ReverseWordsInString
             BellmanFordAsMap
             RemoveNthNodeFromEndOfList
             MinRewards
@@ -132,6 +130,8 @@ public class Test {
             SpiralMatrixTraverse
             ProductOfArrayExceptSelf
             LongestIncreasingPathInMatrix
+            DeleteNodeInLinkedList
+            ReverseWordsInString
          */
     }
 }
