03de5a03d83778562ac0853b3f4c7988ace42b7f
sanantoha <sanantoha@gmail.com>
2022-12-22 18:38:33 +0400

    +2 tasks 22.12.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            BellmanFordAsMap
-            RemoveNthNodeFromEndOfList
             MinRewards
             WordBreak
             IntersectionLinkedList
@@ -132,6 +130,8 @@ public class Test {
             LongestIncreasingPathInMatrix
             DeleteNodeInLinkedList
             ReverseWordsInString
+            BellmanFordAsMap
+            RemoveNthNodeFromEndOfList
          */
     }
 }
