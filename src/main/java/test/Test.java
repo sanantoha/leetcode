c74ed1e04d1a8ccf032a3345ee489633e8713401
sanantoha <sanantoha@gmail.com>
2022-12-24 12:57:04 +0400

    +2 tasks 24.12.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            IntersectionLinkedList
-            LRUCache
             MergeSort
             CloneGraph
             MinHeap
@@ -132,6 +130,8 @@ public class Test {
             RemoveNthNodeFromEndOfList
             MinRewards
             WordBreak
+            IntersectionLinkedList
+            LRUCache
          */
     }
 }
