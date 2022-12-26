0fdd9064db6be4476a6754b0fa6d8c4c89d1f540
sanantoha <sanantoha@gmail.com>
2022-12-26 14:05:19 +0400

    +2 tasks 26.12.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            MinHeap
-            MinNumberOfCoinsForChange
             QuickSort
             Powerset
             AddTwoNumbers
@@ -132,6 +130,8 @@ public class Test {
             LRUCache
             MergeSort
             CloneGraph
+            MinHeap
+            MinNumberOfCoinsForChange
          */
     }
 }
