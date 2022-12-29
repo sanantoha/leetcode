b7199eac79e6c68b57e62df1ccb2615234d7184e
sanantoha <sanantoha@gmail.com>
2022-12-29 19:28:43 +0400

    +2 tasks 29.12.2022.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            RiverSizes
-            Sqrt
             TopKFrequentWords
             CountingSort
             DFSTreeTraverseRec
@@ -132,6 +130,8 @@ public class Test {
             Powerset
             AddTwoNumbers
             MergeIntervals
+            RiverSizes
+            Sqrt
          */
     }
 }
