77275dd369cfbadb9c5a976acade938f36539d1f
sanantoha <sanantoha@gmail.com>
2023-01-15 14:26:48 +0400

    +2 tasks 15.01.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            PrimMinSpanningTree
-            KruskalMinSpanningTree
             BSTSuccessorSearch
             StableInternships
             SameBSTs
@@ -135,6 +133,8 @@ public class Test {
             KthSmallestElementInArray
             BestTimeToBuyAndSellStock
             BinarySearch
+            PrimMinSpanningTree
+            KruskalMinSpanningTree
          */
     }
 }
