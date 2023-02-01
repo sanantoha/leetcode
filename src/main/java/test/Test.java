b5a8e8f016372088db732ad0f59fe75fdfabfdc7
sanantoha <sanantoha@gmail.com>
2023-02-01 11:11:47 +0400

    +2 tasks 01.02.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            PrimMinSpanningTreeAsMap
-            SymmetricTree
             MinNumberOfJumps
             SparseMatrixMultiplication
             LongestNonDecrSubseq
@@ -136,6 +134,8 @@ public class Test {
             ReconstructBST
             DijkstraShortestPathsAsMap
             MinimalHeaviestSetA
+            PrimMinSpanningTreeAsMap
+            SymmetricTree
          */
     }
 }
