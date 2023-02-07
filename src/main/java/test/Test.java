b491610aff9ed5f73ce1b8b316ca4d427c1fc665
sanantoha <sanantoha@gmail.com>
2023-02-07 21:39:48 +0400

    +2 tasks 07.02.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            DiskStacking
-            LargestRange
             LowestCommonAncestorOfBinaryTree
             SelectSort
             AllPathsFromSourceTarget
@@ -137,6 +135,8 @@ public class Test {
             DFSTreeTraverse
             KruskalMinSpanningTreeAsMap
             CycleLinkedList
+            DiskStacking
+            LargestRange
          */
     }
 }
