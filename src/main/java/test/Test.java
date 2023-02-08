7c249b0e4a0654678475c4b7275fec647ea66778
sanantoha <sanantoha@gmail.com>
2023-02-08 10:11:27 +0400

    +2 tasks 08.02.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            LowestCommonAncestorOfBinaryTree
-            SelectSort
             AllPathsFromSourceTarget
             DepthFirstSearchAsMap
             PhoneNumberMnemonic
@@ -137,6 +135,8 @@ public class Test {
             CycleLinkedList
             DiskStacking
             LargestRange
+            LowestCommonAncestorOfBinaryTree
+            SelectSort
          */
     }
 }
