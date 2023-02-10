17707fd88d15c2e2ef2237e59bc2ff1197e6b0a7
sanantoha <sanantoha@gmail.com>
2023-02-10 11:25:54 +0400

    +2 tasks 10.02.2023

--- src/main/java/backtracking/PhoneNumberMnemonic1.java
+++ src/main/java/backtracking/PhoneNumberMnemonic1.java
@@ -11,6 +11,7 @@ public class PhoneNumberMnemonic1 {
     public static void main(String[] args) {
         String str = "1905";
 
+        // [1w0j, 1w0k, 1w0l, 1x0j, 1x0k, 1x0l, 1y0j, 1y0k, 1y0l, 1z0j, 1z0k, 1z0l]
         System.out.println(phoneNumberMnemonics(str));
     }
 }
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            PhoneNumberMnemonic
-            FourSum
             SubarraySumEqualsK
             NumberOfWaysToMakeChange
             WordSearch
@@ -137,6 +135,8 @@ public class Test {
             SelectSort
             AllPathsFromSourceTarget
             DepthFirstSearchAsMap
+            PhoneNumberMnemonic
+            FourSum
          */
     }
 }
