3b049f66cbcfcba2263db0ebed8b7e2d9adbe70e
sanantoha <sanantoha@gmail.com>
2023-02-11 12:37:20 +0400

    +2 tasks 11.02.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            SubarraySumEqualsK
-            NumberOfWaysToMakeChange
             WordSearch
             ClimbingStairs
             NextGreaterElement
@@ -137,6 +135,8 @@ public class Test {
             DepthFirstSearchAsMap
             PhoneNumberMnemonic
             FourSum
+            SubarraySumEqualsK
+            NumberOfWaysToMakeChange
          */
     }
 }
