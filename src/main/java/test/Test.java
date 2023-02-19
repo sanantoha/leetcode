ecb6300abe13780e07cbeed89d8373a9c27cd3c2
sanantoha <sanantoha@gmail.com>
2023-02-19 14:09:53 +0400

    +2 tasks 19.02.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            RotateImage
-            SortList
             Pow
             KnapsackProblem
             ReverseString
@@ -137,6 +135,8 @@ public class Test {
             FirstDuplicateValue
             DijkstraShortestPaths
             PalindromicSubstrings
+            RotateImage
+            SortList
          */
     }
 }
