d95a4913e0c3aa0b00717d17526ab3ec22278e6b
sanantoha <sanantoha@gmail.com>
2023-02-26 13:39:27 +0400

    +2 tasks 26.02.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            SearchInRotatedSortedArray
-            SurroundedRegions
             SpiralMatrixTraverse
             ProductOfArrayExceptSelf
             LongestIncreasingPathInMatrix
@@ -137,6 +135,8 @@ public class Test {
             WordLadderII
             Permutations
             LeftViewBinaryTree
+            SearchInRotatedSortedArray
+            SurroundedRegions
          */
     }
 }
