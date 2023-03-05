e54c5199c00bb41079d7657217dcd7b0c29e2022
sanantoha <sanantoha@gmail.com>
2023-03-05 14:16:49 +0400

    +2 tasks 05.03.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            WordBreak
-            MergeSort
             CloneGraph
             MinHeap
             MinNumberOfCoinsForChange
@@ -137,6 +135,8 @@ public class Test {
             MinRewards
             IntersectionLinkedList
             LRUCache
+            WordBreak
+            MergeSort
          */
     }
 }
