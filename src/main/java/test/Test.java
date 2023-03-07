17e5171b77aabd7eb115cd614457c6b25e2677f9
sanantoha <sanantoha@gmail.com>
2023-03-07 21:28:00 +0400

    +2 tasks 07.03.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            MinNumberOfCoinsForChange
-            QuickSort
             Powerset
             AddTwoNumbers
             MergeIntervals
@@ -137,6 +135,8 @@ public class Test {
             MergeSort
             CloneGraph
             MinHeap
+            MinNumberOfCoinsForChange
+            QuickSort
          */
     }
 }
