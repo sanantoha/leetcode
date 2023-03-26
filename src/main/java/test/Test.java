47f8d1723733d9665f93e1d978a643707216e47e
sanantoha <sanantoha@gmail.com>
2023-03-26 13:27:56 +0400

    +2 tasks 26.03.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            PrimMinSpanningTree
-            KruskalMinSpanningTree
             BSTSuccessorSearch
             StableInternships
             SameBSTs
@@ -120,6 +118,7 @@ public class Test {
             MinSwapsTransformStringToPalindrome
             StringWithoutAAAorBBB
             BFSTreeTraverse
+            PrimMinSpanningTree
             UniquePaths
             BinaryTreeDiameter
             ValidIPAddress
@@ -137,6 +136,7 @@ public class Test {
             KthSmallestElementInArray
             BestTimeToBuyAndSellStock
             BinarySearch
+            KruskalMinSpanningTree
          */
     }
 }
