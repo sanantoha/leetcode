f736bdca0ac51577cb459d292e138c7d6659831a
sanantoha <sanantoha@gmail.com>
2023-04-04 17:35:03 +0400

    +2 tasks 04.04.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            MinimumPassesOfMatrix
-            HeapSort
             FindNodesDistanceK
             LongestDecrSubseq
             AllElementsInTwoBinarySearchTrees
@@ -137,6 +135,8 @@ public class Test {
             SameTree
             SearchForRange
             MinimumWindowSubstring
+            MinimumPassesOfMatrix
+            HeapSort
          */
     }
 }
