3a105adca9b35db98d33380b8fe6f848bade3ccd
sanantoha <sanantoha@gmail.com>
2023-04-06 20:30:16 +0400

    +2 tasks 06.04.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            AllElementsInTwoBinarySearchTrees
-            MaxSumIncreasingSubsequence
             BellmanFord
             BranchSums
             FirstPermutationIsSubstrSecondStr
@@ -137,6 +135,8 @@ public class Test {
             HeapSort
             FindNodesDistanceK
             LongestDecrSubseq
+            AllElementsInTwoBinarySearchTrees
+            MaxSumIncreasingSubsequence
          */
     }
 }
