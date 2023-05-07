ef17d23d2cd0ae23c58903c8644a7870b024c7f9
sanantoha <sanantoha@gmail.com>
2023-05-07 13:06:47 +0400

    +2 tasks 07.05.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            SortList
-            SubarraySumEqualsK
             PhoneNumberMnemonic
             Pow
             KnapsackProblem
@@ -140,6 +138,8 @@ public class Test {
             PalindromicSubstrings
             RotateImage
             OneEdit
+            SortList
+            SubarraySumEqualsK
          */
     }
 }
