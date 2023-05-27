1b83116f63e4b86df43e9406a86f42f7d7b824c0
sanantoha <sanantoha@gmail.com>
2023-05-27 11:49:47 +0400

    +2 tasks 27.05.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            MergeIntervals
-            RiverSizes
             Sqrt
             TopKFrequentWords
             FirstDuplicateValue
@@ -140,6 +138,8 @@ public class Test {
             QuickSort
             Powerset
             AddTwoNumbers
+            MergeIntervals
+            RiverSizes
          */
     }
 }
