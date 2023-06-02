e18d5eb8ab6c53753443837eb86faa13e674cb92
sanantoha <sanantoha@gmail.com>
2023-06-02 11:27:25 +0400

    +2 tasks 02.06.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            HouseRobber
-            WordLadder
             MinSwapsTransformStringToPalindrome
             StringWithoutAAAorBBB
             BFSTreeTraverse
@@ -140,6 +138,8 @@ public class Test {
             SubarraySort
             BreadthSearchFirst
             ValidateStartingCity
+            HouseRobber
+            WordLadder
          */
     }
 }
