a532010efcfec824f58f8b89c54c948424c339ca
sanantoha <sanantoha@gmail.com>
2023-06-10 14:13:05 +0400

    +2 tasks 10.06.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            ReverseBinaryTree
-            ReverseLinkedList
             LongestIncreasingPathInMatrix
             InsertSort
             MergeBinaryTrees
@@ -140,6 +138,8 @@ public class Test {
             MedianOfTwoSortedArrays
             BinaryTreeZigzagLevelOrderTraverse
             DepthFirstSearch
+            ReverseBinaryTree
+            ReverseLinkedList
          */
     }
 }
--- src/main/java/tree/ReverseBinaryTree1.java
+++ src/main/java/tree/ReverseBinaryTree1.java
@@ -1,40 +1,13 @@
 package tree;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class ReverseBinaryTree1 {
 
-    // O(n) time | O(h) space
     public static void reverse(TreeNode root) {
-        if (root == null) return;
 
-        TreeNode left = root.left;
-        TreeNode right = root.right;
-        root.left = right;
-        root.right = left;
-        reverse(root.left);
-        reverse(root.right);
     }
 
-    // O(n) time | O(h) space
     public static void reverseIter(TreeNode root) {
-        if (root == null) return;
 
-        Deque<TreeNode> stack = new LinkedList<>();
-        stack.push(root);
-
-        while (!stack.isEmpty()) {
-            TreeNode curr = stack.pop();
-            if (curr == null) continue;
-
-            TreeNode l = curr.left;
-            TreeNode r = curr.right;
-            curr.right = l;
-            curr.left = r;
-            stack.push(curr.left);
-            stack.push(curr.right);
-        }
     }
 
     public static void main(String[] args) {
