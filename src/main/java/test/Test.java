304d1bb2397b5c620e0cd0e29fbaf82b785f5826
sanantoha <sanantoha@gmail.com>
2023-06-23 10:12:25 +0400

    +2 tasks 23.06.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            AStarAlgo
-            SameTree
             SearchForRange
             MinimumWindowSubstring
             MinimumPassesOfMatrix
@@ -140,6 +138,8 @@ public class Test {
             LowestCommonAncestorOfBinarySearchTree
             LongestIncrSubSeq
             ValidateBinarySearchTree
+            AStarAlgo
+            SameTree
          */
     }
 }
