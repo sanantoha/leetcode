33eec7c90ee0f22f3e0faf12e99921a4aea6a356
sanantoha <sanantoha@gmail.com>
2023-06-27 12:41:38 +0400

    +2 tasks 27.06.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            LongestDecrSubseq
-            AllElementsInTwoBinarySearchTrees
             MaxSumIncreasingSubsequence
             BellmanFord
             BranchSums
@@ -140,6 +138,8 @@ public class Test {
             MinimumPassesOfMatrix
             HeapSort
             MinSwapsTransformStringToPalindrome
+            LongestDecrSubseq
+            AllElementsInTwoBinarySearchTrees
          */
     }
 }
