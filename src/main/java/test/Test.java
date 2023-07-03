c8d95735a27a86dcedd801fcf419a9efe73742bb
sanantoha <sanantoha@gmail.com>
2023-07-03 09:05:54 +0400

    +2 tasks 03.07.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            DijkstraShortestPathsAsMap
-            MinNumberOfCoinsForChange
             TopKFrequentWords
             MinimalHeaviestSetA
             PrimMinSpanningTreeAsMap
@@ -140,6 +138,8 @@ public class Test {
             ZigZagTraverse
             PalindromeLinkedList
             TopologicalSortDFSCycleGraph
+            DijkstraShortestPathsAsMap
+            MinNumberOfCoinsForChange
          */
     }
 }
