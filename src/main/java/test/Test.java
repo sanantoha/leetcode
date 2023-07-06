45b9413a0b00c0cc987c84ee9da675d15148fb43
sanantoha <sanantoha@gmail.com>
2023-07-06 08:01:22 +0400

    +2 tasks 06.07.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            SymmetricTree
-            FindNodesDistanceK
             MinNumberOfJumps
             SparseMatrixMultiplication
             LongestNonDecrSubseq
@@ -140,6 +138,8 @@ public class Test {
             TopKFrequentWords
             MinimalHeaviestSetA
             PrimMinSpanningTreeAsMap
+            SymmetricTree
+            FindNodesDistanceK
          */
     }
 }
