f7b96236906760bc8fe8ea4f33e5c68734f42e96
sanantoha <sanantoha@gmail.com>
2023-07-08 09:04:26 +0400

    +2 tasks 08.07.2023

--- src/main/java/dynamic/LongestNonDecrSubseq.java
+++ src/main/java/dynamic/LongestNonDecrSubseq.java
@@ -85,8 +85,6 @@ public class LongestNonDecrSubseq {
                 int j = binarySearch(res, arr[i]);
                 if (j < 0) {
                     j = -(j + 1);
-                } else {
-                    j++;
                 }
                 res.set(j, arr[i]);
             }
@@ -94,19 +92,19 @@ public class LongestNonDecrSubseq {
         return res.size();
     }
 
-    private static int binarySearch(List<Integer> lst, int target) {
+    private static int binarySearch(List<Integer> arr, int target) {
         int l = 0;
-        int r = lst.size() - 1;
+        int r = arr.size() - 1;
 
         while (l <= r) {
             int mid = (l + r) >>> 1;
-            if (target < lst.get(mid)) {
-                r = mid - 1;
-            } else {
+            if (target >= arr.get(mid)) {
                 l = mid + 1;
+            } else {
+                r = mid - 1;
             }
         }
-        return r;
+        return (arr.get(l) == target) ? l : -(l + 1);
     }
 
     // O(n * log(n)) time | O(n) space
