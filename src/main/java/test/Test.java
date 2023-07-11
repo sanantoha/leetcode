5f3d44e8ad9c33472152479c09881384832f8d34
sanantoha <sanantoha@gmail.com>
2023-07-11 07:43:20 +0400

    +2 tasks 11.07.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            KruskalMinSpanningTreeAsMap
-            EvaluateExpressionTree
             DiskStacking
             LargestRange
             LowestCommonAncestorOfBinaryTree
@@ -140,6 +138,8 @@ public class Test {
             DeepCopyArbitraryPointer
             DFSTreeTraverse
             CycleLinkedList
+            KruskalMinSpanningTreeAsMap
+            EvaluateExpressionTree
          */
     }
 }
