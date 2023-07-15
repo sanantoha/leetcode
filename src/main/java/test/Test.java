1b1d2697c7cc0cd30a142e14875a9bcd0035f23e
sanantoha <sanantoha@gmail.com>
2023-07-15 08:42:01 +0400

    +2 tasks 15.07.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            FourSum
-            NumberOfWaysToMakeChange
             WordSearch
             ClimbingStairs
             NextGreaterElement
@@ -140,6 +138,8 @@ public class Test {
             ReconstructBST
             SelectSort
             DepthFirstSearchAsMap
+            FourSum
+            NumberOfWaysToMakeChange
          */
     }
 }
