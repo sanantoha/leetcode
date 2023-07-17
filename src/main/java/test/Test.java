fddeee455f5a2823d7399bebec9ff4252b223324
sanantoha <sanantoha@gmail.com>
2023-07-17 08:28:57 +0400

    +2 tasks 17.07.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            NextGreaterElement
-            ConvertedSortedArrayToBST
             StaircaseTraversal
             MergedTwoSortedLists
             SortKSortedArray
@@ -140,6 +138,8 @@ public class Test {
             NumberOfWaysToMakeChange
             WordSearch
             ClimbingStairs
+            NextGreaterElement
+            ConvertedSortedArrayToBST
          */
     }
 }
