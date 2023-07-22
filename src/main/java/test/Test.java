d341ad8e63f25bc92f122b719dfa361548ea8b4d
sanantoha <sanantoha@gmail.com>
2023-07-22 08:15:59 +0400

    +2 tasks 22.07.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            PalindromicSubstrings
-            RotateImage
             OneEdit
             MinimumWindowSubstring
             SortList
@@ -140,6 +138,8 @@ public class Test {
             LongestCommonSubsequence
             DijkstraShortestPaths
             MinNumberOfJumps
+            PalindromicSubstrings
+            RotateImage
          */
     }
 }
