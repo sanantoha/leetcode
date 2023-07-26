15aa55f6e10bece186052e6a598b9581a48d9d69
sanantoha <sanantoha@gmail.com>
2023-07-26 07:34:22 +0400

    +2 tasks 26.07.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            KnapsackProblem
-            ReverseString
             ConstructBinaryTreeFromPreorderAndInorderTraversal
             BreadthSearchFirstAsMap
             MaximumSubarray
@@ -140,6 +138,8 @@ public class Test {
             SubarraySumEqualsK
             PhoneNumberMnemonic
             Pow
+            KnapsackProblem
+            ReverseString
          */
     }
 }
