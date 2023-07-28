c25b0dd81a6efd00d577ddd4831b70624b3ba45f
sanantoha <sanantoha@gmail.com>
2023-07-28 08:15:49 +0400

    +2 tasks 28.07.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            MaximumSubarray
-            ThreeSum
             BalancedBrackets
             WordLadderII
             WordBreak
@@ -140,6 +138,8 @@ public class Test {
             ReverseString
             ConstructBinaryTreeFromPreorderAndInorderTraversal
             BreadthSearchFirstAsMap
+            MaximumSubarray
+            ThreeSum
          */
     }
 }
