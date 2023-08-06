6fc3aadaba7a331a412a00978b9bc48ded2f2501
sanantoha <sanantoha@gmail.com>
2023-08-06 09:21:39 +0400

    +2 tasks 06.08.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            IntersectionLinkedList
-            LRUCache
             StringWithoutAAAorBBB
             MiddleNode
             MergeSort
@@ -140,6 +138,8 @@ public class Test {
             BellmanFordAsMap
             RemoveNthNodeFromEndOfList
             MinRewards
+            IntersectionLinkedList
+            LRUCache
          */
     }
 }
