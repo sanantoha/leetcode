ef8701772a0b39d825eb0bc0e9a2a749aa048bbf
sanantoha <sanantoha@gmail.com>
2023-08-17 07:45:30 +0400

    +2 tasks 17.08.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            ValidateStartingCity
-            HouseRobber
             WordBreak
             WordLadder
             BFSTreeTraverse
@@ -140,6 +138,8 @@ public class Test {
             SubarraySort
             UniquePathIII
             BreadthSearchFirst
+            ValidateStartingCity
+            HouseRobber
          */
     }
 }
