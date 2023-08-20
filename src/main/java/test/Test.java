55d49922f6fe21ed52721325d810eb7c04770b27
sanantoha <sanantoha@gmail.com>
2023-08-20 10:11:50 +0400

    +2 tasks 20.08.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            PrimMinSpanningTree
-            UniquePaths
             BinaryTreeDiameter
             ValidIPAddress
             PopulatingNextRightPointerInEachNode
@@ -140,6 +138,8 @@ public class Test {
             WordBreak
             WordLadder
             BFSTreeTraverse
+            PrimMinSpanningTree
+            UniquePaths
          */
     }
 }
