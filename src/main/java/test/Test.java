b0d3c5a1094a65821284fccc32a53a000249fd32
sanantoha <sanantoha@gmail.com>
2023-08-29 07:15:13 +0400

    +2 tasks 29.08.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            StableInternships
-            KthSmallestElementInArray
             BestTimeToBuyAndSellStock
             KruskalMinSpanningTree
             BSTSuccessorSearch
@@ -140,6 +138,8 @@ public class Test {
             BinarySearch
             LevenshteinDistance
             BalancedBinaryTree
+            StableInternships
+            KthSmallestElementInArray
          */
     }
 }
