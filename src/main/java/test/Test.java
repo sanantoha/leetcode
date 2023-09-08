31481149edd0156246a8648f968b9db69744a05f
sanantoha <sanantoha@gmail.com>
2023-09-08 08:44:59 +0400

    +2 tasks 08.09.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,7 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            HeapSort
             MinSwapsTransformStringToPalindrome
             LongestDecrSubseq
             AllElementsInTwoBinarySearchTrees
@@ -140,6 +139,7 @@ public class Test {
             ValidateBinarySearchTree
             SameTree
             MinimumPassesOfMatrix
+            HeapSort
          */
     }
 }
