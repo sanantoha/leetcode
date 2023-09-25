d2f894376d5273c83a4e61be2ada9324b52a85e7
sanantoha <sanantoha@gmail.com>
2023-09-25 07:10:42 +0400

    +2 tasks 25.09.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            LargestRange
-            LowestCommonAncestorOfBinaryTree
             ReconstructBST
             AStarAlgo
             SelectSort
@@ -140,6 +138,8 @@ public class Test {
             KruskalMinSpanningTreeAsMap
             EvaluateExpressionTree
             DiskStacking
+            LargestRange
+            LowestCommonAncestorOfBinaryTree
          */
     }
 }
