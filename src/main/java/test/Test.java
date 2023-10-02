0da5d9081495abe408fd05b8575aef83edecde53
sanantoha <sanantoha@gmail.com>
2023-10-02 07:00:32 +0400

    +2 tasks 02.10.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            SortKSortedArray
-            TopologicalSortDFSCycleGraphAsMap
             ReverseInteger
             LongestCommonSubsequence
             DijkstraShortestPaths
@@ -140,6 +138,8 @@ public class Test {
             ConvertedSortedArrayToBST
             StaircaseTraversal
             MergedTwoSortedLists
+            SortKSortedArray
+            TopologicalSortDFSCycleGraphAsMap
          */
     }
 }
