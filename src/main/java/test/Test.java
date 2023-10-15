62fa74f7a77e95da6636dcaf0c2cf9c694443225
sanantoha <sanantoha@gmail.com>
2023-10-15 09:57:50 +0400

    +2 tasks 15.10.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            LeftViewBinaryTree
-            SearchInRotatedSortedArray
             SurroundedRegions
             SpiralMatrixTraverse
             ProductOfArrayExceptSelf
@@ -140,6 +138,8 @@ public class Test {
             Permutations
             MaxPathSumInBinaryTree
             MinNumberOfJumps
+            LeftViewBinaryTree
+            SearchInRotatedSortedArray
          */
     }
 }
