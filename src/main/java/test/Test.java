a8a023dd43d011574ff80f0cad67aeaac44dd304
sanantoha <sanantoha@gmail.com>
2023-10-20 08:26:38 +0400

    +2 tasks 20.10.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,8 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         /*
-            MinRewards
-            IntersectionLinkedList
             LRUCache
             SubarraySumEqualsK
             StringWithoutAAAorBBB
@@ -140,6 +138,8 @@ public class Test {
             ReverseWordsInString
             BellmanFordAsMap
             RemoveNthNodeFromEndOfList
+            MinRewards
+            IntersectionLinkedList
          */
     }
 }
