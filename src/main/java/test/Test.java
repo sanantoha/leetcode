bd9f5521da1fff3cda53f9ec8af0fb5b08c95da6
sanantoha <sanantoha@gmail.com>
2023-11-06 08:46:59 +0400

    +2 tasks 06.11.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -5,8 +5,6 @@ import java.util.Comparator;
 public class Test {
     public static void main(String[] args) {
         /*
-            PopulatingNextRightPointerInEachNode
-            MedianOfTwoSortedArrays
             BinaryTreeZigzagLevelOrderTraverse
             DepthFirstSearch
             ReverseBinaryTree
@@ -144,6 +142,8 @@ public class Test {
             BinaryTreeDiameter
             NumberOfIsland
             ValidIPAddress
+            PopulatingNextRightPointerInEachNode
+            MedianOfTwoSortedArrays
          */
 
         /*
