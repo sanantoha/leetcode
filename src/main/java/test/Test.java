692e30a6cd00fc7095270892785f87c666c24173
sanantoha <sanantoha@gmail.com>
2023-12-18 08:24:01 +0400

    +2 tasks 18.12.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -5,8 +5,6 @@ public class Test {
     public static void main(String[] args) {
 
         /*
-            LongestCommonSubsequence
-            PalindromicSubstrings
             DijkstraShortestPaths
             SearchForRange
             RotateImage
@@ -146,6 +144,8 @@ public class Test {
             SortKSortedArray
             TopologicalSortDFSCycleGraphAsMap
             ReverseInteger
+            LongestCommonSubsequence
+            PalindromicSubstrings
          */
 
         /*
