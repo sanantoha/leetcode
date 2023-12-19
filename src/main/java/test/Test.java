ef6683a8c0573f1bbf4301af3cf8425c26cee45e
sanantoha <sanantoha@gmail.com>
2023-12-19 08:20:09 +0400

    +2 tasks 19.12.2023

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -5,8 +5,6 @@ public class Test {
     public static void main(String[] args) {
 
         /*
-            DijkstraShortestPaths
-            SearchForRange
             RotateImage
             OneEdit
             PrimMinSpanningTreeAsMap
@@ -146,6 +144,8 @@ public class Test {
             ReverseInteger
             LongestCommonSubsequence
             PalindromicSubstrings
+            DijkstraShortestPaths
+            SearchForRange
          */
 
         /*
