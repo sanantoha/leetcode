51b367592f6ad395fb1c097a1f6857ce754dd0b3
sanantoha <sanantoha@gmail.com>
2024-01-01 12:35:07 +0400

    +2 tasks 01.01.2024

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -42,7 +42,6 @@ public class Test {
         );
 
         List<String> medium = List.of(
-                "ReverseWordsInString",
                 "RemoveNthNodeFromEndOfList",
                 "SubarraySumEqualsK",
                 "MergeIntervals",
@@ -75,11 +74,11 @@ public class Test {
                 "OneEdit",
                 "OptimalFreelancing",
                 "Permutations",
-                "ProductOfArrayExceptSelf"
+                "ProductOfArrayExceptSelf",
+                "ReverseWordsInString"
         );
 
         List<String> hardTasks = List.of(
-                "BellmanFordAsMap",
                 "MinRewards",
                 "LRUCache",
                 "StringWithoutAAAorBBB",
@@ -152,152 +151,9 @@ public class Test {
                 "MinNumberOfJumps",
                 "SearchInRotatedSortedArray",
                 "SurroundedRegions",
-                "SpiralMatrixTraverse"
+                "SpiralMatrixTraverse",
+                "BellmanFordAsMap"
         );
-        /*
-            ReverseWordsInString
-            BellmanFordAsMap
-            RemoveNthNodeFromEndOfList
-            MinRewards
-            IntersectionLinkedList
-            LRUCache
-            SubarraySumEqualsK
-            StringWithoutAAAorBBB
-            MiddleNode
-            MergeSort
-            CloneGraph
-            MinHeap
-            FindPivotIndex
-            QuickSort
-            Powerset
-            AddTwoNumbers
-            MinimumWindowSubstring
-            MergeIntervals
-            RiverSizes
-            ConstructBinaryTreeFromPreorderAndInorderTraversal
-            Sqrt
-            RegularExpressionMatch
-            CountingSort
-            DFSTreeTraverseRec
-            SubarraySort
-            UniquePathIII
-            BreadthSearchFirst
-            ValidateStartingCity
-            HouseRobber
-            WordLadder
-            BFSTreeTraverse
-            PrimMinSpanningTree
-            UniquePaths
-            BinaryTreeDiameter
-            NumberOfIsland
-            ValidIPAddress
-            PopulatingNextRightPointerInEachNode
-            MedianOfTwoSortedArrays
-            BinaryTreeZigzagLevelOrderTraverse
-            DepthFirstSearch
-            ReverseBinaryTree
-            ReverseLinkedList
-            StableInternships
-            BinarySearch
-            LevenshteinDistance
-            BalancedBinaryTree
-            KthSmallestElementInArray
-            BestTimeToBuyAndSellStock
-            KruskalMinSpanningTree
-            BSTSuccessorSearch
-            SameBSTs
-            MaxSumIncreasingSubsequence
-            RotateArray
-            NodeDepths
-            WordBreak
-            CombinationSum
-            LowestCommonAncestorOfBinarySearchTree
-            LongestIncrSubSeq
-            ValidateBinarySearchTree
-            LongestIncreasingPathInMatrix
-            SameTree
-            LargestIsland
-            FirstDuplicateValue
-            TopologicalSort
-            MinimumPassesOfMatrix
-            HeapSort
-            MinSwapsTransformStringToPalindrome
-            LongestDecrSubseq
-            AllElementsInTwoBinarySearchTrees
-            BellmanFord
-            BranchSums
-            FirstPermutationIsSubstrSecondStr
-            GenerateParentheses
-            WaterArea
-            InsertSort
-            AllPathsFromSourceTarget
-            FindClosestValueInBST
-            ZigZagTraverse
-            PalindromeLinkedList
-            TopologicalSortDFSCycleGraph
-            MinNumberOfCoinsForChange
-            DijkstraShortestPathsAsMap
-            TopKFrequentWords
-            MinimalHeaviestSetA
-            FindNodesDistanceK
-            StringPermutation
-            LongestNonDecrSubseq
-            FirstUniqueCharacterInString
-            DFSTreeTraverse
-            MergeBinaryTrees
-            DeepCopyArbitraryPointer
-            SymmetricTree
-            CycleLinkedList
-            KruskalMinSpanningTreeAsMap
-            DiskStacking
-            LargestRange
-            LowestCommonAncestorOfBinaryTree
-            SelectSort
-            AStarAlgo
-            ReconstructBST
-            DepthFirstSearchAsMap
-            FourSum
-            NumberOfWaysToMakeChange
-            WordSearch
-            ClimbingStairs
-            NextGreaterElement
-            EvaluateExpressionTree
-            ConvertedSortedArrayToBST
-            StaircaseTraversal
-            MergedTwoSortedLists
-            SortKSortedArray
-            TopologicalSortDFSCycleGraphAsMap
-            ReverseInteger
-            LongestCommonSubsequence
-            PalindromicSubstrings
-            DijkstraShortestPaths
-            SearchForRange
-            RotateImage
-            OneEdit
-            PrimMinSpanningTreeAsMap
-            Pow
-            SortList
-            PhoneNumberMnemonic
-            KnapsackProblem
-            ReverseString
-            ----------------
-            BreadthSearchFirstAsMap
-            MaximumSubarray
-            SparseMatrixMultiplication
-            OptimalFreelancing
-            ThreeSum
-            BalancedBrackets
-            WordLadderII
-            Permutations
-            MaxPathSumInBinaryTree
-            MinNumberOfJumps
-            LeftViewBinaryTree
-            SearchInRotatedSortedArray
-            SurroundedRegions
-            SpiralMatrixTraverse
-            ProductOfArrayExceptSelf
-            DeleteNodeInLinkedList
-         */
 
         /*
             getClass
