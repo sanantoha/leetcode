eba3d5e836b2f31c4f09d17ded9f3784c8619f3a
sanantoha <sanantoha@gmail.com>
2024-01-17 07:14:25 +0400

    +3 tasks 17.01.2024

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -6,7 +6,6 @@ public class Test {
 
     public static void main(String[] args) {
         List<String> easyTasks = List.of(
-                "FirstDuplicateValue",
                 "InsertSort",
                 "FirstUniqueCharacterInString",
                 "SymmetricTree",
@@ -38,11 +37,11 @@ public class Test {
                 "BestTimeToBuyAndSellStock",
                 "LowestCommonAncestorOfBinarySearchTree",
                 "ValidateBinarySearchTree",
-                "SameTree"
+                "SameTree",
+                "FirstDuplicateValue"
         );
 
         List<String> medium = List.of(
-                "KthSmallestElementInArray",
                 "BSTSuccessorSearch",
                 "RotateArray",
                 "NodeDepths",
@@ -78,11 +77,11 @@ public class Test {
                 "BinaryTreeDiameter",
                 "DepthFirstSearch",
                 "ReverseBinaryTree",
-                "ReverseLinkedList"
+                "ReverseLinkedList",
+                "KthSmallestElementInArray"
         );
 
         List<String> hardTasks = List.of(
-                "UniquePathIII",
                 "ValidateStartingCity",
                 "WordLadder",
                 "PrimMinSpanningTree",
@@ -154,7 +153,8 @@ public class Test {
                 "ConstructBinaryTreeFromPreorderAndInorderTraversal",
                 "RegularExpressionMatch",
                 "SameBSTs",
-                "ThreeSum"
+                "ThreeSum",
+                "UniquePathIII"
         );
 
         /*
