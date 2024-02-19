74ef6b1deb5dc3ce14a3d8d0b1b0e3501a5a0813
sanantoha <sanantoha@gmail.com>
2024-02-19 08:30:30 +0400

    +3 tasks 19.02.2024

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -6,7 +6,6 @@ public class Test {
 
     public static void main(String[] args) {
         List<String> easyTasks = List.of(
-                "ValidateBinarySearchTree",
                 "SameTree",
                 "FirstDuplicateValue",
                 "GreatestCommonDivisor",
@@ -38,11 +37,11 @@ public class Test {
                 "BinarySearch",
                 "BalancedBinaryTree",
                 "BestTimeToBuyAndSellStock",
-                "LowestCommonAncestorOfBinarySearchTree"
+                "LowestCommonAncestorOfBinarySearchTree",
+                "ValidateBinarySearchTree"
         );
 
         List<String> medium = List.of(
-                "BreadthSearchFirst",
                 "UniquePaths",
                 "BinaryTreeDiameter",
                 "DepthFirstSearch",
@@ -79,11 +78,11 @@ public class Test {
                 "Sqrt",
                 "QuickSort",
                 "CountingSort",
-                "DFSTreeTraverseRec"
+                "DFSTreeTraverseRec",
+                "BreadthSearchFirst"
         );
 
         List<String> hardTasks = List.of(
-                "MergeBinaryTrees",
                 "DeepCopyArbitraryPointer",
                 "KruskalMinSpanningTreeAsMap",
                 "LargestIsland",
@@ -154,7 +153,8 @@ public class Test {
                 "TopologicalSortDFSCycleGraph",
                 "DijkstraShortestPathsAsMap",
                 "FindNodesDistanceK",
-                "LongestNonDecrSubseq"
+                "LongestNonDecrSubseq",
+                "MergeBinaryTrees"
         );
 
         /*
