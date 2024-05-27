4129d683c9b0479c4ffd2e5d9b2e8deb390d3bcd
sanantoha <sanantoha@gmail.com>
2024-05-27 06:44:58 +0400

    +3 tasks 27.05.2024

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -8,7 +8,6 @@ public class Test {
 
     public static void main(String[] args) {
         List<String> easyTasks = List.of(
-                "BFSTreeTraverse",
                 "PopulatingNextRightPointerInEachNode",
                 "BinaryTreeZigzagLevelOrderTraverse",
                 "BinarySearch",
@@ -40,11 +39,11 @@ public class Test {
                 "FindPivotIndex",
                 "AddTwoNumbers",
                 "HouseRobber",
-                "GreatestCommonDivisor"
+                "GreatestCommonDivisor",
+                "BFSTreeTraverse"
         );
 
         List<String> medium = List.of(
-                "NodeDepths",
                 "LevelOrderBinaryTreeTraverse",
                 "WordBreak",
                 "EvaluateExpressionTree",
@@ -87,11 +86,11 @@ public class Test {
                 "Search2DMatrix",
                 "KthSmallestElementInBST",
                 "BSTSuccessorSearch",
-                "RotateArray"
+                "RotateArray",
+                "NodeDepths"
         );
 
         List<String> hardTasks = List.of(
-                "SortList",
                 "PhoneNumberMnemonic",
                 "KnapsackProblem",
                 "SparseMatrixMultiplication",
@@ -161,7 +160,8 @@ public class Test {
                 "DijkstraShortestPaths",
                 "SearchForRange",
                 "RotateImage",
-                "PrimMinSpanningTreeAsMap"
+                "PrimMinSpanningTreeAsMap",
+                "SortList"
         );
 
         /*
