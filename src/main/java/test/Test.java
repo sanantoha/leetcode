13c48bd5471e21ec2010aafc468b838a3bc7e5c5
AntonSakharovAE <anton@alefeducation.com>
2025-05-07 07:13:18 +0400

    +3 tasks 07.05.2025

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -11,7 +11,6 @@ public class Test {
 
     public static void main(String[] args) {
         List<String> easyTasks = List.of(
-                "MinimumAbsoluteDifferenceInBST",
                 "ReverseString",
                 "MaximumSubarray",
                 "BreadthSearchFirstAsMap",
@@ -44,7 +43,8 @@ public class Test {
                 "GreatestCommonDivisor",
                 "ConvertedSortedArrayToBST",
                 "ReverseInteger",
-                "Pow"
+                "Pow",
+                "MinimumAbsoluteDifferenceInBST"
         );
 
         List<String> medium = List.of(
