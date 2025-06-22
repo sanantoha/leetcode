23306548209f28941102af94890cf10fb5ad4810
AntonSakharovAE <anton@alefeducation.com>
2025-06-22 13:02:00 +0400

    test commit 22.06.2025

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -91,7 +91,6 @@ public class Test {
         );
 
         List<String> hardTasks = List.of(
-                "RotateImage",
                 "PrimMinSpanningTreeAsMap",
                 "SortList",
                 "PhoneNumberMnemonic",
@@ -163,7 +162,8 @@ public class Test {
                 "TopologicalSortDFSCycleGraphAsMap",
                 "LongestCommonSubsequence",
                 "DijkstraShortestPaths",
-                "SearchForRange"
+                "SearchForRange",
+                "RotateImage"
         );
 
         /*
