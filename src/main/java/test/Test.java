7f4e20b5b9825f315b92af63092c6d8e4b5ba792
AntonSakharovAE <anton@alefeducation.com>
2025-07-29 08:46:49 +0400

    +1 task completed 29.07.2025.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -6,7 +6,6 @@ public class Test {
 
     public static void main(String[] args) {
         List<String> easyTasks = List.of(
-                "BestTimeToBuyAndSellStock",
                 "ValidateBinarySearchTree",
                 "SameTree",
                 "FirstDuplicateValue",
@@ -39,7 +38,8 @@ public class Test {
                 "PopulatingNextRightPointerInEachNode",
                 "BinaryTreeZigzagLevelOrderTraverse",
                 "BinarySearch",
-                "BalancedBinaryTree"
+                "BalancedBinaryTree",
+                "BestTimeToBuyAndSellStock"
         );
 
         List<String> medium = List.of(
