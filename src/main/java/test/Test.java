98a532d6b136bf79c6f9deabaf14c5b24984e0b7
AntonSakharovAE <anton@alefeducation.com>
2025-09-14 12:18:59 +0400

    +1 task completed 14.09.2025.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -92,7 +92,6 @@ public class Test {
         );
 
         List<String> hardTasks = List.of(
-                "CloneGraph",
                 "LargestIsland",
                 "MinHeap",
                 "Powerset",
@@ -164,7 +163,8 @@ public class Test {
                 "BellmanFordAsMap",
                 "ReconstructBST",
                 "MinRewards",
-                "MergeSort"
+                "MergeSort",
+                "CloneGraph"
         );
 
         /*
