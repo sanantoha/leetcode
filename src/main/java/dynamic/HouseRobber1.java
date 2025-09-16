991f192e9b42d6bb10374dcbd61629c78fe7ae79
AntonSakharovAE <anton@alefeducation.com>
2025-09-16 07:04:22 +0400

    +1 task completed 16.09.2025.

--- src/main/java/dynamic/HouseRobber1.java
+++ src/main/java/dynamic/HouseRobber1.java
@@ -2,25 +2,8 @@ package dynamic;
 
 public class HouseRobber1 {
 
-    // O(n) time | O(1) space
     public static int rob(int[] arr) {
-        if (arr == null || arr.length == 0) {
-            return 0;
-        }
-        if (arr.length == 1) {
-            return arr[0];
-        }
-
-        int fst = arr[0];
-        int snd = Math.max(arr[0], arr[1]);
-
-        for (int i = 2; i < arr.length; i++) {
-            int tmp = Math.max(fst + arr[i], snd);
-            fst = snd;
-            snd = tmp;
-        }
-
-        return snd;
+        return -1;
     }
 
     public static void main(String[] args) {
