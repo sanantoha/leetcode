49c641092f4fbea0f66bd0d6bcee8772939e9a15
AntonSakharovAE <anton@alefeducation.com>
2025-09-18 06:40:55 +0400

    +1 task completed 18.09.2025.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -6,7 +6,6 @@ public class Test {
 
     public static void main(String[] args) {
         List<String> easyTasks = List.of(
-                "PopulatingNextRightPointerInEachNode",
                 "BinaryTreeZigzagLevelOrderTraverse",
                 "BinarySearch",
                 "BalancedBinaryTree",
@@ -39,7 +38,8 @@ public class Test {
                 "FindPivotIndex",
                 "AddTwoNumbers",
                 "HouseRobber",
-                "BFSTreeTraverse"
+                "BFSTreeTraverse",
+                "PopulatingNextRightPointerInEachNode"
         );
 
         List<String> medium = List.of(
