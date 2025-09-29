fca16cfefdc70bb59f8f9e5abf17cac2ec043c9e
AntonSakharovAE <anton@alefeducation.com>
2025-09-29 11:11:20 +0400

    +1 task completed 29.09.2025.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -92,7 +92,6 @@ public class Test {
         );
 
         List<String> hardTasks = List.of(
-                "MinimumWindowSubstring",
                 "OddEvenLinkedList",
                 "RiverSizes",
                 "SameBSTs",
@@ -164,7 +163,8 @@ public class Test {
                 "LargestIsland",
                 "MinHeap",
                 "Powerset",
-                "MinNumberOfJumps"
+                "MinNumberOfJumps",
+                "MinimumWindowSubstring"
         );
 
         /*
