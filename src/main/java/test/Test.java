cb5d934687958491d45138da0e96aac271e60ede
AntonSakharovAE <anton@alefeducation.com>
2025-10-19 10:12:21 +0400

    +1 task completed 19.10.2025.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -6,7 +6,6 @@ public class Test {
 
     public static void main(String[] args) {
         List<String> easyTasks = List.of(
-                "ConvertedSortedArrayToBST",
                 "ReverseInteger",
                 "Pow",
                 "MinimumAbsoluteDifferenceInBST",
@@ -39,7 +38,8 @@ public class Test {
                 "LowestCommonAncestorOfBinarySearchTree",
                 "NumberOfWaysToMakeChange",
                 "ClimbingStairs",
-                "GreatestCommonDivisor"
+                "GreatestCommonDivisor",
+                "ConvertedSortedArrayToBST"
         );
 
         List<String> medium = List.of(
