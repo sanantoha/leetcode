e024d6a5b33c5620a1deb7455280b102559a0c41
AntonSakharovAE <anton@alefeducation.com>
2025-11-02 09:25:37 +0400

    +1 task completed 02.11.2025.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -7,7 +7,6 @@ public class Test {
     // ApartmentHunting
     public static void main(String[] args) {
         List<String> easyTasks = List.of(
-                "LeftViewBinaryTree",
                 "DeleteNodeInLinkedList",
                 "IntersectionLinkedList",
                 "MiddleNode",
@@ -40,7 +39,8 @@ public class Test {
                 "ReverseString",
                 "MaximumSubarray",
                 "BreadthSearchFirstAsMap",
-                "BalancedBrackets"
+                "BalancedBrackets",
+                "LeftViewBinaryTree"
         );
 
         List<String> medium = List.of(
