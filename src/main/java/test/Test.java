54b9e0c1bdaf3a71a6fdf805c205c35d4eb96e52
sanantoha <sanantoha@gmail.com>
2025-12-24 07:44:58 +0400

    +1 task completed.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -92,7 +92,6 @@ public class Test {
         );
 
         List<String> hardTasks = List.of(
-                "MaxSumIncreasingSubsequence",
                 "KruskalMinSpanningTree",
                 "LongestIncrSubSeq",
                 "StableInternships",
@@ -167,7 +166,8 @@ public class Test {
                 "SubarraySort",
                 "MaxPathSumInBinaryTree",
                 "FindModeInBst",
-                "MedianOfTwoSortedArrays"
+                "MedianOfTwoSortedArrays",
+                "MaxSumIncreasingSubsequence"
         );
 
         /*
