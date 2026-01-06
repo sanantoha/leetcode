dcefa2f2a457b986c9181fd06c6d99e096275238
sanantoha <sanantoha@gmail.com>
2026-01-06 07:01:15 +0400

    +1 task completed.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -6,7 +6,6 @@ public class Test {
 
     public static void main(String[] args) {
         List<String> easyTasks = List.of(
-                "ReverseInteger",
                 "Pow",
                 "MinimumAbsoluteDifferenceInBST",
                 "ReverseString",
@@ -39,7 +38,8 @@ public class Test {
                 "NumberOfWaysToMakeChange",
                 "ClimbingStairs",
                 "GreatestCommonDivisor",
-                "ConvertedSortedArrayToBST"
+                "ConvertedSortedArrayToBST",
+                "ReverseInteger"
         );
 
         List<String> medium = List.of(
