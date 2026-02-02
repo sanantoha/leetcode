6ff644071f56167c33c5c3ceda634aaa7054b245
sanantoha <sanantoha@gmail.com>
2026-02-02 07:10:05 +0400

    +1 task completed.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -92,7 +92,6 @@ public class Test {
         );
 
         List<String> hardTasks = List.of(
-                "HeapSort",
                 "LongestDecrSubseq",
                 "AllElementsInTwoBinarySearchTrees",
                 "BellmanFord",
@@ -169,7 +168,8 @@ public class Test {
                 "LevenshteinDistance",
                 "LongestIncreasingPathInMatrix",
                 "TopologicalSort",
-                "MinimumPassesOfMatrix"
+                "MinimumPassesOfMatrix",
+                "HeapSort"
         );
 
         /*
